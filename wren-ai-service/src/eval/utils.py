483342cf0c199df3a4675c00123417c6fb73367e
ChihYu Yeh <chihyu.jimmy.yeh@gmail.com>
2024-03-14 22:32:14 +0800

    add script to generate text-to-sql eval dataset from spider dataset

--- wren-ai-service/src/eval/utils.py
+++ wren-ai-service/src/eval/utils.py
@@ -1,5 +1,6 @@
 import json
 import os
+import re
 import sqlite3
 import subprocess
 import zipfile
@@ -10,6 +11,7 @@ from typing import Any, Dict, List
 import gdown
 import pandas as pd
 import sqlglot
+import sqlparse
 from tqdm import tqdm
 from tqdm.contrib import tzip
 
@@ -418,3 +420,381 @@ def get_latest_prediction_outputs_file(path: Path, dataset_name: str) -> str:
         return ""
 
     return str(sorted(files, key=_extract_datetime, reverse=True)[0])
+
+
+def transpile_sql_from_sqlite_to_trino(sql_query: str):
+    return sqlglot.transpile(
+        sql_query, read=sqlglot.Dialects.SQLITE, write=sqlglot.Dialects.TRINO
+    )[0]
+
+
+def get_database_names() -> list[str]:
+    return [
+        folder.name for folder in Path("spider/database").iterdir() if folder.is_dir()
+    ]
+
+
+def get_table_names(db_path: str) -> list[str]:
+    # Connect to the SQLite database
+    conn = sqlite3.connect(db_path)
+
+    # Create a cursor object
+    cur = conn.cursor()
+
+    # Get the table names
+    cur.execute("SELECT name FROM sqlite_master WHERE type='table' ORDER BY name")
+
+    # Fetch the results
+    results = cur.fetchall()
+
+    cur.close()
+
+    conn.close()
+
+    return [result[0] for result in results]
+
+
+def get_database_schema(
+    db_path: str, table_names: list[str], should_save_file: bool = False
+) -> list[dict]:
+    # Connect to the SQLite database
+    conn = sqlite3.connect(db_path)
+
+    # Create a cursor object
+    cur = conn.cursor()
+
+    # Get the table schemas
+    table_schemas = []
+    for table_name in table_names:
+        cur.execute(
+            f"SELECT sql FROM sqlite_master WHERE type='table' AND name='{table_name}'"
+        )
+        row = cur.fetchone()
+        if row is not None:
+            table_schema = row[0]
+            table_schemas.append(
+                {
+                    "table_name": table_name,
+                    "table_schema": re.sub(r"\s+", " ", table_schema).strip(),
+                }
+            )
+        else:
+            print(f"No table named {table_name} found.")
+
+    cur.close()
+
+    if should_save_file:
+        file_name = db_path.split("/")[-1].split(".")[0]
+        with open(f"{file_name}_schema.txt", "w") as file:
+            for table_schema in table_schemas:
+                file.write(f"Table name: {table_schema['table_name']}\n")
+                file.write(f"Table schema: {table_schema['table_schema']}\n\n")
+
+    return table_schemas
+
+
+def get_table_relationships(db_path: str):
+    # Connect to the SQLite database
+    conn = sqlite3.connect(db_path)
+    cursor = conn.cursor()
+
+    # Get a list of tables in the database
+    cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
+    tables = [row[0] for row in cursor.fetchall()]
+
+    # Function to check if a column is part of a unique or primary key
+    def is_unique_or_pk(table, column):
+        cursor.execute(f"PRAGMA table_info('{table}')")
+        for col in cursor.fetchall():
+            # Check if the column is a primary key or part of a unique constraint
+            if col[1] == column and (col[5] > 0 or col[3]):
+                return True
+        return False
+
+    # Analyze relationships
+    relationships = {}
+    for table in tables:
+        cursor.execute(f"PRAGMA foreign_key_list('{table}')")
+        fk_list = cursor.fetchall()
+
+        if not fk_list:
+            continue
+
+        for fk in fk_list:
+            ref_table = fk[2]
+            from_column = fk[3]
+            to_column = fk[4]
+
+        # Determine relationship type
+        if is_unique_or_pk(table, from_column):
+            if is_unique_or_pk(ref_table, to_column):
+                relation_type = "ONE_TO_ONE"
+            else:
+                relation_type = "ONE_TO_MANY"
+        else:
+            if is_unique_or_pk(ref_table, to_column):
+                relation_type = "MANY_TO_ONE"
+            else:
+                relation_type = "MANY_TO_MANY"
+
+        relationships[(table, ref_table)] = relation_type
+
+    conn.close()
+    return relationships
+
+
+def get_appropriat_column_type(column_type: str):
+    if column_type.lower() == "text":
+        return "VARCHAR"
+    elif column_type.lower() == "numeric":
+        return "REAL"
+    elif column_type.lower() == "int":
+        return "INTEGER"
+
+    return column_type.upper()
+
+
+def split_table_defnition(table_definition: str):
+    parts = []
+    bracket_level = 0
+    current = []
+    for char in table_definition:
+        if char == "(" and bracket_level == 0:
+            bracket_level += 1
+        elif char == "(":
+            bracket_level += 1
+            current.append(char)
+        elif char == ")" and bracket_level == 1:
+            bracket_level -= 1
+        elif char == ")":
+            bracket_level -= 1
+            current.append(char)
+        elif char == "," and bracket_level == 0:
+            parts.append("".join(current).strip())
+            current = []
+        else:
+            current.append(char)
+    parts.append("".join(current).strip())  # add the last part
+    return parts
+
+
+def parse_column_definition(column_definition: str):
+    column_def = column_definition.split(" ")
+
+    return {
+        "name": column_def[0],
+        "type": column_def[1] if len(column_def) > 1 else "TEXT",
+        "not_null": True
+        if len(column_def) == 3 and column_def[2].lower() == "not null"
+        else False,
+    }
+
+
+def parse_table_definition(
+    table_name: str, table_definition: str, relationships_info: dict
+):
+    match = re.search(r"\((.*)\)", table_definition)
+    assert match
+    inside_parentheses = match.group(1)
+    parts = split_table_defnition(inside_parentheses)
+
+    # Lists to store columns and foreign keys
+    columns = []
+    relationships = []
+    primary_key = ""
+
+    for part in parts:
+        # Checking if the part is a foreign key
+        if part.startswith("foreign key") or part.startswith("FOREIGN KEY"):
+            part = part.replace("`", "").replace('"', "")
+            regex1 = r"FOREIGN KEY \(([^)]+)\) REFERENCES ([^(]+)\(([^)]+)\)"
+            regex2 = r"foreign key \(([^)]+)\) references ([^(]+)\(([^)]+)\)"
+            match = re.search(regex1, part)
+            match2 = re.search(regex2, part)
+            match_result = False
+
+            if match:
+                match_result = match
+            elif match2:
+                match_result = match2
+
+            if match_result:
+                relationships.append(
+                    {
+                        "table_name": table_name,
+                        "foreign_key": match_result.group(1),
+                        "ref_table_name": match_result.group(2),
+                        "ref_column": match_result.group(3),
+                        "join_type": relationships_info[
+                            (table_name, match_result.group(2))
+                        ],
+                        "properties": {},
+                    }
+                )
+        elif "PRIMARY KEY" in part or "primary key" in part:
+            regex1 = r'primary key \("(.+?)"\)'
+            regex2 = r'PRIMARY KEY \("(.+?)"\)'
+            match = re.search(regex1, part)
+            match2 = re.search(regex2, part)
+            match_result = False
+
+            if match:
+                match_result = match
+            elif match2:
+                match_result = match2
+
+            if match_result:
+                primary_key = match_result.group(1)
+        else:
+            # Splitting the column name and type
+            column_def = parse_column_definition(part)
+
+            columns.append(
+                {
+                    "name": column_def["name"].replace('"', ""),
+                    "type": get_appropriat_column_type(column_def["type"]),
+                    "notNull": column_def[
+                        "not_null"
+                    ],  # Assuming notNull is False by default as not specified in the string
+                    "isCalculated": False,  # Assuming isCalculated is False by default
+                    "expression": column_def["name"].replace(
+                        '"', ""
+                    ),  # Assuming expression is the column name itself
+                    "properties": {},
+                }
+            )
+
+    if relationships:
+        for relationship in relationships:
+            columns.append(
+                {
+                    "name": relationship["ref_table_name"],
+                    "type": relationship["ref_table_name"],
+                    "notNull": True,
+                    "isCalculated": False,
+                    "relationship": f"{relationship['table_name']}_{relationship['ref_table_name']}",
+                    "properties": {},
+                }
+            )
+
+    return {
+        "columns": columns,
+        "primary_key": primary_key,
+        "relationships": relationships,
+    }
+
+
+def generate_mdl_json(
+    database_schema: list[dict],
+    catalog_name: str,
+    schema_name: str,
+    database_name: str,
+    relationships_info: dict,
+    should_save_file: bool = False,
+    file_path: str = "",
+):
+    mdl_json = {
+        "catalog": catalog_name,
+        "schema": schema_name,
+        "models": [],
+        "relationships": [],
+        # these will be empty for now
+        "metrics": [],
+        "cumulativeMetrics": [],
+        "enumDefinitions": [],
+        "views": [],
+        "macros": [],
+    }
+
+    for table in database_schema:
+        # remove comments
+        clean_table_schema = table["table_schema"].replace(
+            "-- this should be removed", ""
+        )
+        parsed = sqlparse.parse(clean_table_schema)[0]
+        table_definition = parse_table_definition(
+            table["table_name"], str(parsed.tokens[-1]), relationships_info
+        )
+
+        mdl_json["models"].append(
+            {
+                "name": table["table_name"],
+                "properties": {},
+                "refSql": f"select * from \"{catalog_name}\".{schema_name}.\"{database_name}-{table['table_name']}\"",
+                "columns": table_definition["columns"],
+                "primaryKey": table_definition["primary_key"],
+            }
+        )
+
+        if table_definition["relationships"]:
+            for relationship in table_definition["relationships"]:
+                mdl_json["relationships"].append(
+                    {
+                        "name": f"{relationship['table_name']}_{relationship['ref_table_name']}",
+                        "models": [
+                            relationship["table_name"],
+                            relationship["ref_table_name"],
+                        ],
+                        "joinType": relationship["join_type"],
+                        "condition": f"{relationship['table_name']}.{relationship['foreign_key']} = {relationship['ref_table_name']}.{relationship['ref_column']}",
+                    }
+                )
+
+    if should_save_file:
+        data_root = "src/eval/data"
+        if not Path(data_root).exists():
+            Path(data_root).mkdir(parents=True, exist_ok=True)
+
+        if not file_path:
+            file_path = f"{data_root}/{database_name}.json"
+        # save the file
+        with open(file_path, "w") as file:
+            json.dump(mdl_json, file, indent=2)
+
+        print(
+            f"MDL JSON for {database_name} generated successfully. Check the {data_root} folder."
+        )
+
+    return mdl_json
+
+
+def generate_text_to_sql_dataset(
+    paths: list[str],
+    database_name: str = "baseball_1",
+    should_save_file: bool = False,
+    file_path: str = "data/baseball_1_data.json",
+):
+    target_data = []
+    for path in paths:
+        with open(path, "r") as f:
+            data = json.load(f)
+
+        if database_name == "":
+            target_data += data
+        else:
+            for entry in data:
+                if entry["db_id"] == database_name:
+                    target_data.append(
+                        {
+                            "question": entry["question"],
+                            "answer": transpile_sql_from_sqlite_to_trino(
+                                re.sub(r"\s+", " ", entry["query"]).strip()
+                            ),
+                        }
+                    )
+
+    if should_save_file:
+        data_root = "src/eval/data"
+        if not Path(data_root).exists():
+            Path(data_root).mkdir(parents=True, exist_ok=True)
+
+        with open(file_path, "w") as f:
+            for entry in target_data:
+                json.dump(entry, f)
+                f.write("\n")
+
+        print(
+            f"Dataset for {database_name} is generated successfully. Check the {data_root} folder."
+        )
+
+    return target_data
