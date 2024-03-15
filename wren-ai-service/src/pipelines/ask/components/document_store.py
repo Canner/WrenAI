044ceb5865532a086374eafa997692c158f5f32a
ChihYu Yeh <chihyu.jimmy.yeh@gmail.com>
2024-03-15 09:22:36 +0800

    add todo comment

--- wren-ai-service/src/pipelines/ask/components/document_store.py
+++ wren-ai-service/src/pipelines/ask/components/document_store.py
@@ -13,9 +13,12 @@ def init_document_store(
     env: str = env,
     embedding_dim: int = EMBEDDING_MODEL_DIMENSION,
     dataset_name: Optional[str] = None,
+    recreate_index: bool = False,
 ):
     return QdrantDocumentStore(
         url="localhost" if env == "dev" else "qdrant",
         embedding_dim=embedding_dim,
         index=dataset_name or "Document",
+        recreate_index=recreate_index,
+        # hnsw_config={"ef_construct": 200, "m": 32},  # https://qdrant.tech/documentation/concepts/indexing/#vector-index
     )
