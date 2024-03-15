491aa1d4c0e2d7a1df39e72e539c71fda885d41d
Freda Lai <fredalai00@gmail.com>
2024-03-15 17:55:47 +0800

    feat(wren-ui): remove generate metadata

--- wren-ui/src/components/pages/modeling/MetadataDrawer.tsx
+++ wren-ui/src/components/pages/modeling/MetadataDrawer.tsx
@@ -1,7 +1,6 @@
-import { Drawer, Button } from 'antd';
+import { Drawer } from 'antd';
 import { NODE_TYPE } from '@/utils/enum';
 import { DrawerAction } from '@/hooks/useDrawerAction';
-import { SparklesIcon } from '@/utils/icons';
 import ModelMetadata, {
   Props as ModelMetadataProps,
 } from './metadata/ModelMetadata';
@@ -11,8 +10,6 @@ import MetricMetadata, {
 import ViewMetadata, {
   Props as ViewMetadataProps,
 } from './metadata/ViewMetadata';
-import useModalAction from '@/hooks/useModalAction';
-import GenerateMetadataModal from '@/components/pages/modeling/GenerateMetadataModal';
 
 type Metadata = { nodeType: NODE_TYPE } & ModelMetadataProps &
   MetricMetadataProps &
@@ -24,16 +21,6 @@ export default function MetadataDrawer(props: Props) {
   const { visible, defaultValue, onClose } = props;
   const { referenceName, nodeType = NODE_TYPE.MODEL } = defaultValue || {};
 
-  const generateMetadataModal = useModalAction();
-  const openGeneratedMetadataModal = () => {
-    // TODO: put generated metadata in
-    generateMetadataModal.openModal(defaultValue);
-  };
-
-  const submitGenerateMetadata = async (values) => {
-    console.log(values);
-  };
-
   return (
     <Drawer
       visible={visible}
@@ -42,27 +29,10 @@ export default function MetadataDrawer(props: Props) {
       closable
       destroyOnClose
       onClose={onClose}
-      footer={
-        <div className="text-right">
-          <Button
-            className="d-inline-flex align-center"
-            icon={<SparklesIcon className="mr-2" />}
-            onClick={openGeneratedMetadataModal}
-          >
-            Generate metadata
-          </Button>
-        </div>
-      }
     >
       {nodeType === NODE_TYPE.MODEL && <ModelMetadata {...defaultValue} />}
       {nodeType === NODE_TYPE.METRIC && <MetricMetadata {...defaultValue} />}
       {nodeType === NODE_TYPE.VIEW && <ViewMetadata {...defaultValue} />}
-
-      <GenerateMetadataModal
-        {...generateMetadataModal.state}
-        onClose={generateMetadataModal.closeModal}
-        onSubmit={submitGenerateMetadata}
-      />
     </Drawer>
   );
 }
