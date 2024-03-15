9818d19df1510ceb79c8aac6780ee0326afcd6c6
Pao-Sheng Wang <paooap.oappao@gmail.com>
2024-03-15 19:01:48 +0800

    run the example for the ragas evaluator

--- wren-ai-service/src/eval/ask_details.py
+++ wren-ai-service/src/eval/ask_details.py
@@ -14,7 +14,45 @@
 #      a. implement with ragas evaluator
 #      b. according to the evaluation result to run the CTE query to ensure it is equal to the input query
 #    3. aggregate the evaluation result and generate the evaluation report
+from haystack import Pipeline
+from haystack_integrations.components.evaluators.ragas import (
+    RagasEvaluator,
+    RagasMetric,
+)
 
+from src.eval.utils import download_spider_data
+from src.utils import load_env_vars
 
 if __name__ == "__main__":
-    pass
+    download_spider_data()
+    load_env_vars()
+
+    pipeline = Pipeline()
+    evaluator = RagasEvaluator(
+        metric=RagasMetric.CONTEXT_RELEVANCY,
+    )
+    pipeline.add_component("evaluator", evaluator)
+
+    results = pipeline.run(
+        {
+            "evaluator": {
+                "questions": [
+                    "When was the Rhodes Statue built?",
+                    "Where is the Pyramid of Giza?",
+                ],
+                "contexts": [["Context for question 1"], ["Context for question 2"]],
+            }
+        }
+    )
+results = pipeline.run(
+    {
+        "evaluator": {
+            "questions": [
+                "When was the Rhodes Statue built?",
+                "Where is the Pyramid of Giza?",
+            ],
+            "contexts": [["Context for question 1"], ["Context for question 2"]],
+        }
+    }
+)
+pass
