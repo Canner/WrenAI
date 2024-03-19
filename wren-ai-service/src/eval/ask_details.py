fc6d0419edfa96153bcfa9eaf37199882bd19ce8
Pao-Sheng Wang <paooap.oappao@gmail.com>
2024-03-19 17:35:23 +0800

    produce the eval context

--- wren-ai-service/src/eval/ask_details.py
+++ wren-ai-service/src/eval/ask_details.py
@@ -14,45 +14,63 @@
 #      a. implement with ragas evaluator
 #      b. according to the evaluation result to run the CTE query to ensure it is equal to the input query
 #    3. aggregate the evaluation result and generate the evaluation report
-from haystack import Pipeline
-from haystack_integrations.components.evaluators.ragas import (
-    RagasEvaluator,
-    RagasMetric,
-)
+import json
+import os
 
-from src.eval.utils import download_spider_data
 from src.utils import load_env_vars
 
-if __name__ == "__main__":
-    download_spider_data()
-    load_env_vars()
 
-    pipeline = Pipeline()
-    evaluator = RagasEvaluator(
-        metric=RagasMetric.CONTEXT_RELEVANCY,
-    )
-    pipeline.add_component("evaluator", evaluator)
+def _prepare_ask_details_eval_data():
+    with open("./data/baseball_1_data.json") as f:
+        inputs = [json.loads(line) for line in f]
 
-    results = pipeline.run(
+    eval_context = [
         {
-            "evaluator": {
-                "questions": [
-                    "When was the Rhodes Statue built?",
-                    "Where is the Pyramid of Giza?",
+            "input": {"query": i["question"], "sql": i["answer"], "summary": None},
+            "output": {
+                "description": None,
+                "steps": [
+                    {
+                        "sql": None,
+                        "summary": None,
+                        "cte_name": None,
+                    }
                 ],
-                "contexts": [["Context for question 1"], ["Context for question 2"]],
-            }
+            },
         }
-    )
-results = pipeline.run(
-    {
-        "evaluator": {
-            "questions": [
-                "When was the Rhodes Statue built?",
-                "Where is the Pyramid of Giza?",
-            ],
-            "contexts": [["Context for question 1"], ["Context for question 2"]],
-        }
-    }
-)
-pass
+        for i in inputs
+    ]
+
+    # ensure the directory exists
+    os.makedirs("./data/ask_details/", exist_ok=True)
+
+    # save the context to json file
+    with open("./data/ask_details/baseball_1_eval_context.json", "w") as f:
+        json.dump(eval_context, f)
+    pass
+
+
+if __name__ == "__main__":
+    load_env_vars()
+    _prepare_ask_details_eval_data()
+
+    # pipeline = Pipeline()
+    # evaluator = RagasEvaluator(
+    #     metric=RagasMetric.CONTEXT_RELEVANCY,
+    # )
+    # pipeline.add_component("evaluator", evaluator)
+    #
+    # results = pipeline.run(
+    #     {
+    #         "evaluator": {
+    #             "questions": [
+    #                 "When was the Rhodes Statue built?",
+    #                 "Where is the Pyramid of Giza?",
+    #             ],
+    #             "contexts": [["Context for question 1"], ["Context for question 2"]],
+    #         }
+    #     }
+    # )
+    #
+    # print(results)
+    pass
