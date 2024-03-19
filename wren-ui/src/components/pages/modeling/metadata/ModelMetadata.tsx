031815d80220211b260fe5b222b87cf74306b135
Freda Lai <fredalai00@gmail.com>
2024-03-19 10:03:23 +0800

    chore(wren-ui): remove calculated field table import from model metadata

--- wren-ui/src/components/pages/modeling/metadata/ModelMetadata.tsx
+++ wren-ui/src/components/pages/modeling/metadata/ModelMetadata.tsx
@@ -1,9 +1,6 @@
 import { Typography } from 'antd';
 import FieldTable from '@/components/table/FieldTable';
-import CalculatedFieldTable from '@/components/table/CalculatedFieldTable';
 import RelationTable from '@/components/table/RelationTable';
-import { makeMetadataBaseTable } from '@/components/table/MetadataBaseTable';
-import UpdateMetadataModal from '@/components/modals/UpdateMetadataModal';
 
 export interface Props {
   displayName: string;
@@ -16,28 +13,7 @@ export interface Props {
 }
 
 export default function ModelMetadata(props: Props) {
-  const {
-    referenceName,
-    fields = [],
-    calculatedFields = [],
-    relations = [],
-  } = props || {};
-
-  const CalculatedFieldMetadataTable =
-    makeMetadataBaseTable(CalculatedFieldTable)(UpdateMetadataModal);
-
-  // To convert edit value for update metadata modal
-  const editMetadataValue = (value) => {
-    return {
-      displayName: value.displayName || value.name,
-      description: value.properties?.description,
-    };
-  };
-
-  const submitMetadata = (values) => {
-    // TODO: waiting for API
-    console.log(values);
-  };
+  const { referenceName, fields = [], relations = [] } = props || {};
 
   return (
     <>
@@ -52,19 +28,6 @@ export default function ModelMetadata(props: Props) {
         </Typography.Text>
         <FieldTable dataSource={fields} />
       </div>
-
-      {!!calculatedFields.length && (
-        <div className="mb-6">
-          <Typography.Text className="d-block gray-7 mb-2">
-            Calculated fields ({calculatedFields.length})
-          </Typography.Text>
-          <CalculatedFieldMetadataTable
-            dataSource={calculatedFields}
-            onEditValue={editMetadataValue}
-            onSubmitRemote={submitMetadata}
-          />
-        </div>
-      )}
 
       <div className="mb-6">
         <Typography.Text className="d-block gray-7 mb-2">
