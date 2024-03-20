b88215d0b40746d842bd7e78eae714d90161a7c8
ChihYu Yeh <chihyu.jimmy.yeh@gmail.com>
2024-03-20 10:55:33 +0800

    resolve conflict

--- wren-ai-service/src/eval/dataset.py
+++ wren-ai-service/src/eval/dataset.py
@@ -17,7 +17,7 @@ if __name__ == "__main__":
     parser.add_argument(
         "--database_name",
         type=str,
-        required=True,
+        default="book_2",
     )
     args = parser.parse_args()
     database_name = args.database_name
