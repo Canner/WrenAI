bed733157db825b749df12465746bc5ee4fafead
Pao-Sheng Wang <paooap.oappao@gmail.com>
2024-03-21 19:07:12 +0800

    test ragas evaluator

--- wren-ai-service/src/eval/ask_details.py
+++ wren-ai-service/src/eval/ask_details.py
@@ -19,9 +19,8 @@ def _prepare_ask_details_eval_data(input_path: str, output_path: str):
         )
         return
 
-    generator = init_generator()
     generation_pipeline = Generation(
-        generator=generator,
+        generator=init_generator(),
     )
 
     def _generate_data(input: dict):
@@ -50,6 +49,15 @@ def _prepare_ask_details_eval_data(input_path: str, output_path: str):
         json.dump(eval_context, f)
 
 
+def _build_cte_query(steps) -> str:
+    return "".join(
+        f"WITH {step['cte_name']} AS ({step['sql']})\n"
+        if step["cte_name"]
+        else step["sql"]
+        for step in steps
+    )
+
+
 if __name__ == "__main__":
     load_env_vars()
     _prepare_ask_details_eval_data(
@@ -64,36 +72,61 @@ if __name__ == "__main__":
     #    3. aggregate the evaluation result and generate the evaluation report
 
     # read the evaluation data
-    with open("./data/ask_details/baseball_1_eval_context.json") as f:
-        eval_context = json.load(f)
+    # with open("./data/ask_details/baseball_1_eval_context.json") as f:
+    #     eval_context = json.load(f)
+    #
+    # pipeline = Generation(
+    #     generator=init_generator(),
+    # )
+    #
+    # for i, element in enumerate(eval_context):
+    #     if i > 0:  # todo: remove the limit after the process is stable
+    #         break
+    #     response = pipeline.run(
+    #         sql=element["input"]["sql"],
+    #     )
+    #     output = json.loads(response["generator"]["replies"][0])
+    #
+    #     # build the CTE query from response steps
+    #     steps = output["steps"]
+    #     print(steps)
 
-    generator = init_generator()
-    pipeline = Generation(
-        generator=generator,
+    from haystack import Pipeline
+    from haystack_integrations.components.evaluators.ragas import (
+        RagasEvaluator,
+        RagasMetric,
     )
 
-    for item in eval_context:
-        pipeline.run(
-            sql=input["answer"],
-        )
+    pipeline = Pipeline()
+    evaluator_context = RagasEvaluator(
+        metric=RagasMetric.CONTEXT_PRECISION,
+    )
+    pipeline.add_component("evaluator_context", evaluator_context)
 
-    # pipeline = Pipeline()
-    # evaluator = RagasEvaluator(
-    #     metric=RagasMetric.CONTEXT_RELEVANCY,
-    # )
-    # pipeline.add_component("evaluator", evaluator)
-    #
-    # results = pipeline.run(
-    #     {
-    #         "evaluator": {
-    #             "questions": [
-    #                 "When was the Rhodes Statue built?",
-    #                 "Where is the Pyramid of Giza?",
-    #             ],
-    #             "contexts": [["Context for question 1"], ["Context for question 2"]],
-    #         }
-    #     }
-    # )
-    #
-    # print(results)
+    QUESTIONS = [
+        "Which is the most popular global sport?",
+        "Who created the Python language?",
+    ]
+    CONTEXTS = [
+        [
+            "The popularity of sports can be measured in various ways, including TV viewership, social media presence, number of participants, and economic impact. Football is undoubtedly the world's most popular sport with major events like the FIFA World Cup and sports personalities like Ronaldo and Messi, drawing a followership of more than 4 billion people."
+        ],
+        [
+            "Python, created by Guido van Rossum in the late 1980s, is a high-level general-purpose programming language. Its design philosophy emphasizes code readability, and its language constructs aim to help programmers write clear, logical code for both small and large-scale software projects."
+        ],
+    ]
+    GROUND_TRUTHS = [
+        "Football is the most popular sport",
+        "Python language was created by Guido van Rossum.",
+    ]
+    results = pipeline.run(
+        {
+            "evaluator_context": {
+                "questions": QUESTIONS,
+                "contexts": CONTEXTS,
+                "ground_truths": GROUND_TRUTHS,
+            },
+        }
+    )
+    print(results)
     pass
