1476a597e95071f6b3eb3dd656e32c8349bd9fa2
Freda Lai <fredalai00@gmail.com>
2024-03-26 15:05:27 +0800

    feat(wren-ui): add apollo error handler

--- wren-ui/src/apollo/client/index.ts
+++ wren-ui/src/apollo/client/index.ts
@@ -1,11 +1,15 @@
-import { ApolloClient, HttpLink, InMemoryCache } from '@apollo/client';
+import { ApolloClient, HttpLink, InMemoryCache, from } from '@apollo/client';
+import { onError } from '@apollo/client/link/error';
+import errorHandler from '@/utils/errorHandler';
 
-const link = new HttpLink({
+const apolloErrorLink = onError((error) => errorHandler(error));
+
+const httpLink = new HttpLink({
   uri: '/api/graphql',
 });
 
 const client = new ApolloClient({
-  link,
+  link: from([apolloErrorLink, httpLink]),
   cache: new InMemoryCache(),
 });
 
