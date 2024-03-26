b802d261fcb7c9cc2500c5bc38a8a4374d2584bd
onlyjackfrost <onlyjackfrost@gmail.com>
2024-03-26 12:31:40 +0800

    feat(wren-ui): add music sample data initsqls

---
+++ wren-ui/src/apollo/server/data/sample.ts
@@ -0,0 +1,13 @@
+export const sampleData = {
+  music: {
+    initSql: `\
+        CREATE TABLE album AS FROM 'https://wrenai-public.s3.amazonaws.com/demo/Music/Album.csv';
+        CREATE TABLE artist AS FROM 'https://wrenai-public.s3.amazonaws.com/demo/Music/Artist.csv';
+        CREATE TABLE customer AS FROM 'https://wrenai-public.s3.amazonaws.com/demo/Music/Customer.csv';
+        CREATE TABLE genre AS FROM 'https://wrenai-public.s3.amazonaws.com/demo/Music/Genre.csv';
+        CREATE TABLE invoice AS FROM 'https://wrenai-public.s3.amazonaws.com/demo/Music/Invoice.csv';
+        CREATE TABLE invoiceLine AS FROM 'https://wrenai-public.s3.amazonaws.com/demo/Music/InvoiceLine.csv';
+        CREATE TABLE track AS FROM 'https://wrenai-public.s3.amazonaws.com/demo/Music/Track.csv';
+    `,
+  },
+};
---
+++ wren-ui/src/apollo/server/data/type.ts
@@ -0,0 +1,5 @@
+export enum SampleDataName {
+  MUSIC = 'MUSIC',
+  NBA = 'NBA',
+  ECOMMERCE = 'ECOMMERCE',
+}
