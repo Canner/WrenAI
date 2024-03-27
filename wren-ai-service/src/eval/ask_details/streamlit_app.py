0c1639079ab0128a7f0fb39be0c48bc44f94801f
Pao-Sheng Wang <paooap.oappao@gmail.com>
2024-03-27 11:13:47 +0800

    adjust the structure

--- wren-ai-service/src/eval/ask_details/streamlit_app.py
+++ wren-ai-service/src/eval/ask_details/streamlit_app.py
@@ -4,20 +4,34 @@ from pathlib import Path
 import streamlit as st
 
 
-def show_eval_results(eval_file_path: Path):
+def load_eval_results(eval_file_path: str):
     with open(eval_file_path, "r") as f:
         eval_results = json.load(f)
+    return eval_results
 
-    st.markdown("### Performance Benchmark")
+
+def show_eval_results(eval_results: dict):
+    st.markdown("### Metrics")
     for k, v in eval_results.items():
-        st.markdown(f"**{k}**")
         if k == "collection":
-            for record in v:
-                st.json(record, expanded=True)
-        elif isinstance(v, dict):
+            continue
+        st.markdown(f"**{k}**")
+        if isinstance(v, dict):
             st.json(v, expanded=True)
         else:
             st.markdown(v)
+
+
+def show_collection(eval_results: dict):
+    st.markdown("### Collection")
+    collection = eval_results.get("collection", [])
+    for record in collection:
+        for k, v in record.items():
+            st.markdown(f"**{k}**")
+            if isinstance(v, dict):
+                st.json(v, expanded=True)
+            else:
+                st.markdown(v)
 
 
 st.set_page_config(layout="wide")
@@ -45,7 +59,9 @@ if len(selected_timestamps) == 1:
         f"{output_dir}/{dataset_name}_eval_report_{selected_timestamps[0]}.json"
     )
     st.markdown(f"## {selected_timestamps[0]}")
-    show_eval_results(eval_report_1_path)
+    eval_results_1 = load_eval_results(eval_report_1_path)
+    show_eval_results(eval_results_1)
+    show_collection(eval_results_1)
 elif len(selected_timestamps) == 2:
     eval_report_1_path = (
         f"{output_dir}/{dataset_name}_eval_report_{selected_timestamps[0]}.json"
@@ -56,8 +72,12 @@ elif len(selected_timestamps) == 2:
 
     col1, col2 = st.columns(2)
     with col1:
+        eval_results_1 = load_eval_results(eval_report_1_path)
         st.markdown(f"## {selected_timestamps[0]}")
-        show_eval_results(eval_report_1_path)
+        show_eval_results(eval_results_1)
+        show_collection(eval_results_1)
     with col2:
+        eval_results_2 = load_eval_results(eval_report_2_path)
         st.markdown(f"## {selected_timestamps[1]}")
-        show_eval_results(eval_report_2_path)
+        show_eval_results(eval_results_2)
+        show_collection(eval_results_2)
