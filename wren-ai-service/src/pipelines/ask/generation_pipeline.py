1f1f306e509b2d2b788d5242c4f2ede9a063aea7
Pao Sheng <paooap.oappao@gmail.com>
2024-03-28 14:18:09 +0800

    Revert "Feature(wren-ai-service): fix failed sql queries (#39)"
    This reverts commit 87c21e486aa0416c724ab499e3f3da870128dc0c.

--- wren-ai-service/src/pipelines/ask/generation_pipeline.py
+++ wren-ai-service/src/pipelines/ask/generation_pipeline.py
@@ -1,17 +1,23 @@
 import os
+import uuid
 from typing import Any, List, Optional
 
 from haystack import Document, Pipeline
+from haystack.components.builders.prompt_builder import PromptBuilder
 
 from src.core.pipeline import BasicPipeline
+from src.pipelines.ask.components.document_store import init_document_store
+from src.pipelines.ask.components.embedder import init_embedder
 from src.pipelines.ask.components.generator import (
     MODEL_NAME,
     init_generator,
 )
-from src.pipelines.ask.components.post_processor import init_post_processor
-from src.pipelines.ask.components.prompts import init_text_to_sql_prompt_builder
+from src.pipelines.ask.components.post_processor import PostProcessor
+from src.pipelines.ask.components.prompts import generation_user_prompt_template
+from src.pipelines.ask.components.retriever import init_retriever
+from src.pipelines.ask.retrieval_pipeline import Retrieval
 from src.utils import load_env_vars
-from src.web.v1.services.ask import AskRequest
+from src.web.v1.services.ask import AskRequest, AskResultResponse
 
 load_env_vars()
 
@@ -26,28 +32,21 @@ if with_trace := os.getenv("ENABLE_TRACE", default=False):
 class Generation(BasicPipeline):
     def __init__(
         self,
-        text_to_sql_generator: Any,
+        generator: Any,
         with_trace: bool = False,
     ):
         self._pipeline = Pipeline()
         self._pipeline.add_component(
-            "text_to_sql_prompt_builder",
-            init_text_to_sql_prompt_builder(),
+            "prompt_builder", PromptBuilder(template=generation_user_prompt_template)
         )
-        self._pipeline.add_component("text_to_sql_generator", text_to_sql_generator)
-        self._pipeline.add_component("post_processor", init_post_processor())
+        self._pipeline.add_component("generator", generator)
+        self._pipeline.add_component("post_processor", PostProcessor())
 
-        self._pipeline.connect(
-            "text_to_sql_prompt_builder.prompt", "text_to_sql_generator.prompt"
-        )
-        self._pipeline.connect(
-            "text_to_sql_generator.replies", "post_processor.replies"
-        )
+        self._pipeline.connect("prompt_builder.prompt", "generator.prompt")
+        self._pipeline.connect("generator.replies", "post_processor.replies")
 
         self.with_trace = with_trace
-        self.text_to_sql_prompt_builder = self._pipeline.get_component(
-            "text_to_sql_prompt_builder"
-        )
+        self.prompt_builder = self._pipeline.get_component("prompt_builder")
 
         super().__init__(self._pipeline)
 
@@ -69,16 +68,16 @@ class Generation(BasicPipeline):
 
             result = self._pipeline.run(
                 {
-                    "text_to_sql_prompt_builder": {
+                    "prompt_builder": {
                         "query": query,
                         "documents": contexts,
                         "history": history,
                     },
-                    "text_to_sql_generator": {
+                    "generator": {
                         "trace_generation_input": TraceGenerationInput(
                             trace_id=trace.id,
                             name="generator",
-                            input=self.text_to_sql_prompt_builder.run(
+                            input=self.prompt_builder.run(
                                 query=query,
                                 documents=contexts,
                                 history=history,
@@ -89,11 +88,11 @@ class Generation(BasicPipeline):
                 }
             )
 
-            trace.update(input=query, output=result["text_to_sql_generator"])
+            trace.update(input=query, output=result["generator"])
         else:
             result = self._pipeline.run(
                 {
-                    "text_to_sql_prompt_builder": {
+                    "prompt_builder": {
                         "query": query,
                         "documents": contexts,
                         "history": history,
@@ -104,10 +103,54 @@ class Generation(BasicPipeline):
         return result
 
 
+# this is for quick testing only, please ignore this
 if __name__ == "__main__":
-    generation_pipeline = Generation(
-        text_to_sql_generator=init_generator(),
+    DATASET_NAME = os.getenv("DATASET_NAME")
+
+    document_store = init_document_store()
+    embedder = init_embedder(with_trace=with_trace)
+    retriever = init_retriever(document_store=document_store, with_trace=with_trace)
+    generator = init_generator(with_trace=with_trace)
+
+    retrieval_pipeline = Retrieval(
+        embedder=embedder,
+        retriever=retriever,
+        with_trace=with_trace,
     )
 
-    print("generating generation_pipeline.jpg to outputs/pipelines/ask...")
-    generation_pipeline.draw("./outputs/pipelines/ask/generation_pipeline.jpg")
+    generation_pipeline = Generation(
+        generator=generator,
+        with_trace=with_trace,
+    )
+
+    if DATASET_NAME == "book_2":
+        query = "How many books are there?"
+    elif DATASET_NAME == "college_3":
+        query = "Count the number of courses."
+    else:
+        query = "random query here..."
+
+    retrieval_result = retrieval_pipeline.run(
+        query,
+        user_id=str(uuid.uuid4()) if with_trace else None,
+    )
+
+    generation_result = generation_pipeline.run(
+        query,
+        contexts=retrieval_result["retriever"]["documents"],
+        user_id=str(uuid.uuid4()) if with_trace else None,
+    )
+
+    print(generation_result)
+
+    assert AskResultResponse.AskResult(
+        **generation_result["post_processor"]["results"][0]
+    )
+
+    if with_trace:
+        generation_pipeline.draw(
+            "./outputs/pipelines/ask/generation_pipeline_with_trace.jpg"
+        )
+        langfuse.flush()
+    else:
+        generation_pipeline.draw("./outputs/pipelines/ask/generation_pipeline.jpg")
--- wren-ai-service/tests/test_main.py
+++ wren-ai-service/tests/test_main.py
@@ -1,14 +1,16 @@
 import json
 import uuid
 
+import pytest
 from fastapi.testclient import TestClient
 
 from src.__main__ import app
 
-GLOBAL_DATA = {
-    "semantics_preperation_id": str(uuid.uuid4()),
-    "query_id": None,
-}
+
+@pytest.fixture
+def mdl_str():
+    with open("tests/data/book_2_mdl.json", "r") as f:
+        return json.dumps(json.load(f))
 
 
 def test_semantics_description():
@@ -51,12 +53,9 @@ def test_semantics_description():
         )
 
 
-def test_semantics_preparations():
+def test_semantics_preparations(mdl_str: str):
     with TestClient(app) as client:
-        semantics_preperation_id = GLOBAL_DATA["semantics_preperation_id"]
-
-        with open("tests/data/book_2_mdl.json", "r") as f:
-            mdl_str = json.dumps(json.load(f))
+        semantics_preperation_id = str(uuid.uuid4())
 
         response = client.post(
             url="/v1/semantics-preparations/",
@@ -83,15 +82,30 @@ def test_semantics_preparations():
         assert status == "finished"
 
 
-def test_asks():
+def test_asks(mdl_str: str):
     with TestClient(app) as client:
-        semantics_preparation_id = GLOBAL_DATA["semantics_preperation_id"]
+        semantics_preperation_id = str(uuid.uuid4())
+
+        response = client.post(
+            url="/v1/semantics-preparations/",
+            json={
+                "mdl": mdl_str,
+                "id": semantics_preperation_id,
+            },
+        )
+
+        status = "indexing"
+        while status != "finished":
+            response = client.get(
+                url=f"/v1/semantics-preparations/{semantics_preperation_id}/status/"
+            )
+            status = response.json()["status"]
 
         response = client.post(
             url="/v1/asks",
             json={
                 "query": "How many books are there?",
-                "id": semantics_preparation_id,
+                "id": semantics_preperation_id,
             },
         )
 
@@ -99,27 +113,46 @@ def test_asks():
         assert response.json()["query_id"] != ""
 
         query_id = response.json()["query_id"]
-        GLOBAL_DATA["query_id"] = query_id
 
         response = client.get(url=f"/v1/asks/{query_id}/result/")
-        while (
-            response.json()["status"] != "finished"
-            and response.json()["status"] != "failed"
-        ):
+        while response.json()["status"] != "finished":
             response = client.get(url=f"/v1/asks/{query_id}/result/")
 
         assert response.status_code == 200
-        if response.json()["status"] == "failed":
-            assert response.json()["error"]
-        else:
-            for r in response.json()["response"]:
-                assert r["sql"] is not None and r["sql"] != ""
-                assert r["summary"] is not None and r["summary"] != ""
+        assert response.json()["status"] == "finished"
+        for r in response.json()["response"]:
+            assert r["sql"] is not None and r["sql"] != ""
+            assert r["summary"] is not None and r["summary"] != ""
 
 
-def test_stop_asks():
+def test_stop_asks(mdl_str: str):
     with TestClient(app) as client:
-        query_id = GLOBAL_DATA["query_id"]
+        semantics_preperation_id = str(uuid.uuid4())
+
+        response = client.post(
+            url="/v1/semantics-preparations/",
+            json={
+                "mdl": mdl_str,
+                "id": semantics_preperation_id,
+            },
+        )
+
+        status = "indexing"
+        while status != "finished":
+            response = client.get(
+                url=f"/v1/semantics-preparations/{semantics_preperation_id}/status/"
+            )
+            status = response.json()["status"]
+
+        response = client.post(
+            url="/v1/asks",
+            json={
+                "query": "How many books are there?",
+                "id": semantics_preperation_id,
+            },
+        )
+
+        query_id = response.json()["query_id"]
 
         response = client.patch(
             url=f"/v1/asks/{query_id}",
@@ -139,14 +172,49 @@ def test_stop_asks():
         assert response.json()["status"] == "stopped"
 
 
-def test_ask_details():
+def test_ask_details(mdl_str: str):
     with TestClient(app) as client:
+        semantics_preperation_id = str(uuid.uuid4())
+
+        response = client.post(
+            url="/v1/semantics-preparations/",
+            json={
+                "mdl": mdl_str,
+                "id": semantics_preperation_id,
+            },
+        )
+
+        status = "indexing"
+        while status != "finished":
+            response = client.get(
+                url=f"/v1/semantics-preparations/{semantics_preperation_id}/status/"
+            )
+            status = response.json()["status"]
+
+        query = "How many books are there?"
+        response = client.post(
+            url="/v1/asks",
+            json={
+                "query": query,
+                "id": semantics_preperation_id,
+            },
+        )
+
+        query_id = response.json()["query_id"]
+
+        response = client.get(url=f"/v1/asks/{query_id}/result/")
+        while response.json()["status"] != "finished":
+            response = client.get(url=f"/v1/asks/{query_id}/result/")
+
+        sql = response.json()["response"][0]["sql"]
+        summary = response.json()["response"][0]["summary"]
+
         response = client.post(
             url="/v1/ask-details/",
             json={
-                "query": "How many books are there?",
-                "sql": "SELECT COUNT(*) FROM book",
-                "summary": "Retrieve the number of books",
+                "query": query,
+                "sql": sql,
+                "summary": summary,
             },
         )
 
