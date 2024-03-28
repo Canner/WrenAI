c0d601ddc7b8d8fba43196b3c4f3d4ffbe1174c4
ChihYu Yeh <chihyu.jimmy.yeh@gmail.com>
2024-03-28 11:15:42 +0800

    simplify prompt

--- wren-ai-service/src/pipelines/ask/components/prompts.py
+++ wren-ai-service/src/pipelines/ask/components/prompts.py
@@ -5,14 +5,6 @@ You are a Trino SQL expert with exceptional logical thinking skills.
 Print what you think the SQL query should be given the question and the data model. 
 This is vital to my career, I will become homeless if you make a mistake.
 
-### INSTRUCTIONS ###
-- If the question is complex enough, you can also answer complex SQL query that consists of a combination of JOINs, subqueries, and conditional filtering.
-- Try not to use '*' to select all columns, please be specific what columns to choose from the table.
-- If you can't construct the Trino SQL query, please answer with empty SQL string.
-- If you can construct the Trino SQL query, please answer with the SQL query: ```sql ...```.
-- Make sure the chosen "GROUP BY" conditions are correct given the selected columns.
-- If the query history is not empty, please consider the previous query in order to make correct Trino SQL query.
-
 ### TASK ###
 Given an input question, create a syntactically correct Trino SQL query to run and a short sentence within 10 words to summary the Trino SQL query 
 and return them as the answer to the input question.
@@ -25,9 +17,6 @@ and return them as the answer to the input question.
 ### QUERY HISTORY ###
 {{ history }}
 
-### QUESTION ###
-{{ query }}
-
 ### FINAL ANSWER FORMAT ###
 The final answer must be the JSON format
 
@@ -36,6 +25,9 @@ For a question that you can return the SQL query
 
 For a question that you can't return the SQL query
 {"sql": "", "summary": ""}
+
+### QUESTION ###
+{{ query }} Think step by step to generate the Trino SQL query.
 """
 
 sql_correction_user_prompt_template = """
