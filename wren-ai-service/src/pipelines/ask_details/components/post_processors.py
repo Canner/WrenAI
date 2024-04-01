422763fc2ac9b000d4d01255e17ae336d61d4b49
ChihYu Yeh <chihyu.jimmy.yeh@gmail.com>
2024-04-01 08:23:04 +0800

    fix postprocessor for ask details

--- wren-ai-service/src/pipelines/ask_details/components/post_processors.py
+++ wren-ai-service/src/pipelines/ask_details/components/post_processors.py
@@ -18,35 +18,27 @@ class GenerationPostProcessor:
     @component.output_types(
         results=Optional[Dict[str, Any]],
     )
-    def run(self, replies: List[str], meta: List[Dict[str, Any]]) -> Dict[str, Any]:
-        generator = {
-            "replies": replies,
-            "meta": meta,
-        }
-
-        cleaned_generation_result = json.loads(clean_generation_result(replies[0]))
+    def run(self, inputs: List[str]) -> Dict[str, Any]:
+        cleaned_generation_result = json.loads(clean_generation_result(inputs[0]))
 
         steps = cleaned_generation_result.get("steps", [])
         if not steps:
             return {
-                "generator": generator,
-                "results": None,
+                "post_processing_results": None,
             }
 
         sql = _build_cte_query(steps)
 
         if not check_if_sql_executable(os.getenv("WREN_ENGINE_ENDPOINT"), sql):
             return {
-                "generator": generator,
-                "results": None,
+                "post_processing_results": None,
             }
 
         # make sure the last step has an empty cte_name
         cleaned_generation_result["steps"][-1]["cte_name"] = ""
 
         return {
-            "generator": generator,
-            "results": cleaned_generation_result,
+            "post_processing_results": cleaned_generation_result,
         }
 
 
