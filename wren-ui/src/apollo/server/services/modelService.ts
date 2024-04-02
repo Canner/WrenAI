3ae0f88de96e960f77496465236741c88a180817
Andy Yen <38731840+onlyjackfrost@users.noreply.github.com>
2024-04-02 15:42:50 +0800

    avoid deploy multiple time when onboarding with sample dataset (#62)

--- wren-ui/src/apollo/server/services/modelService.ts
+++ wren-ui/src/apollo/server/services/modelService.ts
@@ -1,4 +1,16 @@
+import { DataSourceStrategyFactory } from '../factories/onboardingFactory';
+import {
+  IModelColumnRepository,
+  IModelRepository,
+  IRelationRepository,
+  Model,
+  Relation,
+} from '../repositories';
+import { RelationData } from '../types';
+import { IProjectService } from './projectService';
+
 export interface IModelService {
+  saveRelations(relations: RelationData[]): Promise<Relation[]>;
   generateReferenceName(data: any): string;
 }
 
@@ -9,11 +21,109 @@ export interface GenerateReferenceNameData {
 }
 
 export class ModelService implements IModelService {
+  private projectService: any;
+  private modelRepository: any;
+  private modelColumnRepository: any;
+  private relationRepository: any;
+
+  constructor({
+    projectService,
+    modelRepository,
+    modelColumnRepository,
+    relationRepository,
+  }: {
+    projectService: IProjectService;
+    modelRepository: IModelRepository;
+    modelColumnRepository: IModelColumnRepository;
+    relationRepository: IRelationRepository;
+  }) {
+    this.projectService = projectService;
+    this.modelRepository = modelRepository;
+    this.modelColumnRepository = modelColumnRepository;
+    this.relationRepository = relationRepository;
+  }
+
   public generateReferenceName(data: GenerateReferenceNameData): string {
     const { sourceTableName, existedReferenceNames } = data;
     if (!existedReferenceNames.includes(sourceTableName)) {
       return sourceTableName;
     }
     return `${sourceTableName}_${existedReferenceNames.length + 1}`;
+  }
+
+  public async saveModels(strategy: DataSourceStrategyFactory, tables: any) {
+    const project = await this.projectService.getCurrentProject();
+    const projectId = project.id;
+
+    // delete existing models and columns
+    await this.resetCurrentProjectModel(projectId);
+
+    // create model and columns
+
+    const { models, columns } = await strategy.saveModels(tables);
+  }
+
+  public async saveRelations(relations: RelationData[]) {
+    const project = await this.projectService.getCurrentProject();
+
+    const models = await this.modelRepository.findAllBy({
+      projectId: project.id,
+    });
+
+    const columnIds = relations
+      .map(({ fromColumnId, toColumnId }) => [fromColumnId, toColumnId])
+      .flat();
+    const columns =
+      await this.modelColumnRepository.findColumnsByIds(columnIds);
+    const relationValues = relations.map((relation) => {
+      const fromColumn = columns.find(
+        (column) => column.id === relation.fromColumnId,
+      );
+      if (!fromColumn) {
+        throw new Error(`Column not found, column Id ${relation.fromColumnId}`);
+      }
+      const toColumn = columns.find(
+        (column) => column.id === relation.toColumnId,
+      );
+      if (!toColumn) {
+        throw new Error(`Column not found, column Id  ${relation.toColumnId}`);
+      }
+      const relationName = this.generateRelationName(relation, models);
+      return {
+        projectId: project.id,
+        name: relationName,
+        fromColumnId: relation.fromColumnId,
+        toColumnId: relation.toColumnId,
+        joinType: relation.type,
+      } as Partial<Relation>;
+    });
+
+    const savedRelations = await Promise.all(
+      relationValues.map((relation) =>
+        this.relationRepository.createOne(relation),
+      ),
+    );
+    return savedRelations;
+  }
+
+  private generateRelationName(relation: RelationData, models: Model[]) {
+    const fromModel = models.find((m) => m.id === relation.fromModelId);
+    const toModel = models.find((m) => m.id === relation.toModelId);
+    if (!fromModel || !toModel) {
+      throw new Error('Model not found');
+    }
+    return (
+      fromModel.sourceTableName.charAt(0).toUpperCase() +
+      fromModel.sourceTableName.slice(1) +
+      toModel.sourceTableName.charAt(0).toUpperCase() +
+      toModel.sourceTableName.slice(1)
+    );
+  }
+
+  private async resetCurrentProjectModel(projectId) {
+    const existsModels = await this.modelRepository.findAllBy({ projectId });
+    const modelIds = existsModels.map((m) => m.id);
+    await this.modelColumnRepository.deleteByModelIds(modelIds);
+    await this.modelRepository.deleteMany(modelIds);
   }
 }
