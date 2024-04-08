9f805080fb829b310a41cc3b74f7211d72019d4c
ChihYu Yeh <chihyu.jimmy.yeh@gmail.com>
2024-04-08 16:01:12 +0800

    resolve conflict

--- wren-ai-service/src/eval/utils.py
+++ wren-ai-service/src/eval/utils.py
@@ -2,6 +2,7 @@ def get_generation_model_pricing(
     model_name: str,
 ):
     # https://openai.com/pricing
+    # https://docs.anthropic.com/claude/docs/models-overview#model-comparison
     generation_model_pricing = {
         "gpt-3.5-turbo": {
             "prompt_tokens": 0.5 / 10**6,
@@ -18,6 +19,10 @@ def get_generation_model_pricing(
         "gpt-4-0125-preview": {
             "prompt_tokens": 10 / 10**6,
             "completion_tokens": 30 / 10**6,
+        },
+        "claude-3-haiku-20240307": {
+            "prompt_tokens": 0.25 / 10**6,
+            "completion_tokens": 1.25 / 10**6,
         },
     }
 
