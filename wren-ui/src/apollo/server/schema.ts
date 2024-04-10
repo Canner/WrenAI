2321bee5ed28aae7265d6390e19337e853eafe88
Freda Lai <fredalai00@gmail.com>
2024-04-10 18:17:45 +0800

    feat(wren-ui): add deploy related APIs

--- wren-ui/src/apollo/server/schema.ts
+++ wren-ui/src/apollo/server/schema.ts
@@ -459,7 +459,7 @@ export const typeDefs = gql`
     # Modeling Page
     listModels: [ModelInfo!]!
     model(where: ModelWhereInput!): DetailedModel!
-    modelSync: ModelSyncResponse
+    modelSync: ModelSyncResponse!
     diagram: Diagram!
 
     # View
