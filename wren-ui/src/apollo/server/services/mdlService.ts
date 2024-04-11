07f187450d53d0d97939ad738c4fc84b5f69c64d
william chang(張仲威) <wwwy3y3@gmail.com>
2024-04-12 01:54:11 +0800

    feat(wren-ui): view crud, validation & preview

--- wren-ui/src/apollo/server/services/mdlService.ts
+++ wren-ui/src/apollo/server/services/mdlService.ts
@@ -14,50 +14,50 @@ export interface IMDLService {
 export class MDLService implements IMDLService {
   private projectRepository: IProjectRepository;
   private modelRepository: IModelRepository;
-  private viewRepository: IViewRepository;
   private modelColumnRepository: IModelColumnRepository;
   private relationRepository: IRelationRepository;
+  private viewRepository: IViewRepository;
 
   constructor({
     projectRepository,
     modelRepository,
-    viewRepository,
     modelColumnRepository,
     relationRepository,
+    viewRepository,
   }: {
     projectRepository: IProjectRepository;
     modelRepository: IModelRepository;
-    viewRepository: IViewRepository;
     modelColumnRepository: IModelColumnRepository;
     relationRepository: IRelationRepository;
+    viewRepository: IViewRepository;
   }) {
     this.projectRepository = projectRepository;
     this.modelRepository = modelRepository;
-    this.viewRepository = viewRepository;
     this.modelColumnRepository = modelColumnRepository;
     this.relationRepository = relationRepository;
+    this.viewRepository = viewRepository;
   }
 
   public async makeCurrentModelMDL() {
     const project = await this.projectRepository.getCurrentProject();
     const projectId = project.id;
     const models = await this.modelRepository.findAllBy({ projectId });
-    const views = await this.viewRepository.findAllBy({ projectId });
     const modelIds = models.map((m) => m.id);
     const columns =
       await this.modelColumnRepository.findColumnsByModelIds(modelIds);
     const relations = await this.relationRepository.findRelationInfoBy({
       projectId,
     });
+    const views = await this.viewRepository.findAllBy({ projectId });
     const relatedModels = models;
     const relatedColumns = columns;
     const relatedRelations = relations;
     const mdlBuilder = new MDLBuilder({
       project,
       models,
-      views,
       columns,
       relations,
+      views,
       relatedModels,
       relatedColumns,
       relatedRelations,
