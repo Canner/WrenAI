f1c16dff62da303b881a5e1d7bcd3a2335626cdb
andreashimin <andreashimin@gmail.com>
2024-04-12 11:21:28 +0800

    revert(wren-ui): disable feature cache settings until model provides cache setup

--- wren-ui/src/components/settings/utils.tsx
+++ wren-ui/src/components/settings/utils.tsx
@@ -1,21 +1,12 @@
 import { SETTINGS } from '@/utils/enum';
 import DatabaseOutlined from '@ant-design/icons/DatabaseOutlined';
 import ProjectOutlined from '@ant-design/icons/ProjectOutlined';
-import { LightningIcon } from '@/utils/icons';
 
 export const getSettingMenu = (menu: SETTINGS) =>
   ({
     [SETTINGS.DATA_SOURCE]: {
       icon: DatabaseOutlined,
       label: 'Data Source Settings',
-    },
-    [SETTINGS.CACHE]: {
-      icon: ({ className = undefined }) => (
-        <span role="img" className={`anticon ${className || ''}`}>
-          <LightningIcon />
-        </span>
-      ),
-      label: 'Cache Settings',
     },
     [SETTINGS.PROJECT]: {
       icon: ProjectOutlined,
