99c09e0523edf8bf49deec1d8a91576e452419e7
shimin <shimin@shimins-MacBook-Pro.local>
2024-04-15 14:16:54 +0800

    feat(wren-ui): setup posthog to app

--- wren-ui/src/pages/_app.tsx
+++ wren-ui/src/pages/_app.tsx
@@ -1,12 +1,38 @@
 import { AppProps } from 'next/app';
 import Head from 'next/head';
-import apolloClient from '@/apollo/client';
-import { ApolloProvider } from '@apollo/client';
 import { Spin } from 'antd';
-require('../styles/index.less');
+import env from '@/utils/env';
+import posthog from 'posthog-js';
+import apolloClient from '@/apollo/client';
+import { PostHogProvider } from 'posthog-js/react';
+import { ApolloProvider } from '@apollo/client';
 import { defaultIndicator } from '@/components/PageLoading';
 
+require('../styles/index.less');
+
 Spin.setDefaultIndicator(defaultIndicator);
+
+// Check that PostHog is client-side (used to handle Next.js SSR)
+if (env.isTelemetryEnabled && typeof window !== 'undefined') {
+  posthog.init(env.posthogAPIKey, {
+    api_host: env.posthogHost,
+    autocapture: {
+      dom_event_allowlist: ['click'],
+      css_selector_allowlist: ['[data-ph-capture="true"]'],
+    },
+    session_recording: {
+      maskAllInputs: false,
+      maskInputOptions: {
+        password: true,
+      },
+    },
+    disable_session_recording: env.isDevelopment,
+    // Enable debug mode in development
+    loaded: (posthog) => {
+      if (env.isDevelopment) posthog.debug();
+    },
+  });
+}
 
 function App({ Component, pageProps }: AppProps) {
   return (
@@ -16,9 +42,11 @@ function App({ Component, pageProps }: AppProps) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <ApolloProvider client={apolloClient}>
-        <main className="app">
-          <Component {...pageProps} />
-        </main>
+        <PostHogProvider client={posthog}>
+          <main className="app">
+            <Component {...pageProps} />
+          </main>
+        </PostHogProvider>
       </ApolloProvider>
     </>
   );
---
+++ wren-ui/src/utils/env.ts
@@ -0,0 +1,12 @@
+const env = {
+  isDevelopment: process.env.NODE_ENV === 'development',
+  isProduction: process.env.NODE_ENV === 'production',
+  isTelemetryEnabled:
+    process.env.NEXT_PUBLIC_TELEMETRY_ENABLED &&
+    process.env.NEXT_PUBLIC_TELEMETRY_ENABLED.toLocaleLowerCase() === 'true',
+  posthogAPIKey: process.env.NEXT_PUBLIC_POSTHOG_API_KEY,
+  posthogHost:
+    process.env.NEXT_PUBLIC_POSTHOG_HOST || 'https://app.posthog.com',
+};
+
+export default env;
