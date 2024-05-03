30180cd4f276af2dac0117785edebfa1b9d024ed
onlyjackfrost <onlyjackfrost@gmail.com>
2024-05-03 13:32:43 +0800

    Remove unused code and interfaces from model.ts

--- wren-ui/src/apollo/server/models/model.ts
+++ wren-ui/src/apollo/server/models/model.ts
@@ -1,15 +1,3 @@
-enum _ModelType {
-  TABLE = 'TABLE',
-  CUSTOM = 'CUSTOM',
-}
-
-interface _CalculatedFieldData {
-  name: string;
-  expression: string;
-  lineage: number[];
-  diagram: JSON;
-}
-
 export interface CreateModelData {
   sourceTableName: string;
   fields: [string];
