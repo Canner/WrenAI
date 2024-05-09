8e2bc94fc9fb280d619414a53cb8bd1d9d9e2577
ChihYu Yeh <chihyu.jimmy.yeh@gmail.com>
2024-05-09 16:47:09 +0800

    update

--- wren-ai-service/src/pipelines/sql_explanation/components/prompts.py
+++ wren-ai-service/src/pipelines/sql_explanation/components/prompts.py
@@ -4,8 +4,30 @@ sql_explanation_system_prompt = """
 ### TASK ###
 
 Given the question, sql query, sql analysis to the sql query, sql query summary and full sql query for reference,
-please explain each group of sql analysis result based on sql query.
+please explain each group of sql analysis result based on sql query: how does the expression work, why this expression is given based on the question and why can it answer user's question.
 These are groups of sql analysis results: selectItems, relation, filters, groupByKeys, sortings
+
+For relation type, it's described in sql tree style. Please inspect all relations and list them out.
+For example, the relation tree structure below has two relations that are both INNER_JOIN.
+
+"relation": {
+  "type": "INNER_JOIN",
+  "left": {
+      "type": "INNER_JOIN",
+      "left": {
+        ...
+      },
+      "right": {
+        ...
+      },
+      "criteria": ...
+  },
+  "right": {
+    ...
+  },
+  "criteria": ...
+}
+
 
 ### OUTPUT STRUCTURE ###
 
