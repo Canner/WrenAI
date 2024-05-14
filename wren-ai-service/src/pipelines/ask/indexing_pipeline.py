93bd7fb61e69397a84d1e9852b4367452a679a4e
ChihYu Yeh <chihyu.jimmy.yeh@gmail.com>
2024-05-14 11:35:58 +0800

    fix indexing bug

--- wren-ai-service/src/pipelines/ask/indexing_pipeline.py
+++ wren-ai-service/src/pipelines/ask/indexing_pipeline.py
@@ -83,12 +83,16 @@ class ViewConverter:
         def _format(view: Dict[str, Any]) -> List[str]:
             return str(
                 {
-                    "question": view["properties"]["question"]
-                    if "question" in view["properties"]
-                    else "",
-                    "description": view["properties"]["description"]
-                    if "description" in view["properties"]
-                    else "",
+                    "question": (
+                        view["properties"]["question"]
+                        if "properties" in view and "question" in view["properties"]
+                        else ""
+                    ),
+                    "description": (
+                        view["properties"]["description"]
+                        if "properties" in view and "description" in view["properties"]
+                        else ""
+                    ),
                     "statement": view["statement"],
                 }
             )
