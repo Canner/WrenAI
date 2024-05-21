09cc329301cccef6ae24ed16a91f3dfffe40fd2b
william chang(張仲威) <wwwy3y3@gmail.com>
2024-05-21 13:02:39 +0800

    fix(wren-launcher): fix adding properties to config.properties

--- docker/bootstrap/init.sh
+++ docker/bootstrap/init.sh
@@ -17,18 +17,27 @@ pg_username=$PG_USERNAME
 # touch a empty config.properties if not exists
 # put a content into config.properties if not exists
 if [ ! -f ${data_path}/config.properties ]; then
-    echo "node.environment=production" > ${data_path}/config.properties
-    echo "wren.experimental-enable-dynamic-fields=true" > ${data_path}/config.properties
-#    echo "PG_DATABASE=\"tpch\"" >> ${data_path}/config.properties
+  echo "init config.properties"
+  echo "node.environment=production" > ${data_path}/config.properties
+fi
+
+# after the config.properties is created, check if config properties properly set
+# if not, then append default values to the config.properties
+# check if wren.experimental-enable-dynamic-fields is set, otherwise append it with true
+if ! grep -q "wren.experimental-enable-dynamic-fields" ${data_path}/config.properties; then
+  echo "wren.experimental-enable-dynamic-fields is not set, set it to true"
+  echo "wren.experimental-enable-dynamic-fields=true" >> ${data_path}/config.properties
 fi
 
 # create a folder mdl if not exists
 if [ ! -d ${data_path}/mdl ]; then
+  echo "create mdl folder"
   mkdir ${data_path}/mdl
 fi
 
 # put a emtpy sample.json if not exists
 if [ ! -f ${data_path}/mdl/sample.json ]; then
+  echo "init mdl/sample.json"
   echo "{\"catalog\": \"test_catalog\", \"schema\": \"test_schema\", \"models\": []}" > ${data_path}/mdl/sample.json
 fi
 
@@ -36,5 +45,6 @@ fi
 # accounts is a file contains the authentication information
 # the format is like: `username password` per line
 if [ ! -f ${data_path}/accounts ]; then
+  echo "init accounts file"
   echo "${pg_username} ${pg_password}" > ${data_path}/accounts
 fi
