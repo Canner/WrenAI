9c7bc125167196c8708ea99306e68ad28d93e172
Shimin <andreashimin@gmail.com>
2024-05-22 14:57:27 +0800

    chore(wren-ui): Enhance data source connection error & instructions (#292)
    * chore(wren-ui): throw connection error from data source to graphQL error
    
    * chore(wren-ui): refine data source connection error alert
    
    * chore(wren-ui): provide connection refused error in PG & show instructions to user
    
    * style(wren-ui): adjust error code condition

--- wren-ui/src/components/pages/setup/utils.tsx
+++ wren-ui/src/components/pages/setup/utils.tsx
@@ -11,6 +11,7 @@ import BigQueryProperties from './dataSources/BigQueryProperties';
 import DuckDBProperties from './dataSources/DuckDBProperties';
 import PostgreSQLProperties from './dataSources/PostgreSQLProperties';
 import { SampleDatasetName } from '@/apollo/client/graphql/__types__';
+import { ERROR_CODES } from '@/utils/errorHandler';
 
 type SetupStep = {
   step: number;
@@ -124,4 +125,25 @@ export const getTemplates = () => {
     ...TEMPLATE_OPTIONS[key],
     value: key,
   })) as ButtonOption[];
+};
+
+export const getPostgresErrorMessage = (error: Record<string, any>) => {
+  if (error.code === ERROR_CODES.CONNECTION_REFUSED) {
+    return (
+      <div>
+        {error.message}. <br />
+        If you are having trouble connecting to your PostgreSQL database, please
+        refer to our{' '}
+        <a
+          href="https://docs.getwren.ai/guide/connect/postgresql#connect"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          documentation
+        </a>{' '}
+        for detailed instructions.
+      </div>
+    );
+  }
+  return error.message;
 };
