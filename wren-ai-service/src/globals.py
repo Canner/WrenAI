e4c860f0977aeeefdb08217db83c7f2b4cbd2934
ChihYu Yeh <chihyu.jimmy.yeh@gmail.com>
2024-05-24 15:29:24 +0800

    fix conflicts

--- wren-ai-service/src/globals.py
+++ wren-ai-service/src/globals.py
@@ -23,9 +23,6 @@ from src.pipelines.semantics import description
 from src.pipelines.sql_explanation import (
     generation_pipeline as sql_explanation_generation_pipeline,
 )
-from src.pipelines.sql_explanation.components.prompts import (
-    sql_explanation_system_prompt,
-)
 from src.utils import init_providers
 from src.web.v1.services.ask import AskService
 from src.web.v1.services.ask_details import AskDetailsService
@@ -97,12 +94,11 @@ def init_globals():
             ),
         },
     )
+
     SQL_EXPLANATION_SERVICE = SQLExplanationService(
         pipelines={
             "generation": sql_explanation_generation_pipeline.Generation(
-                generator=llm_provider.get_generator(
-                    system_prompt=sql_explanation_system_prompt,
-                ),
+                llm_provider=llm_provider,
             )
         }
     )
