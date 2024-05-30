ad821779e8ae30407ab49a58ee856c11fd4698f2
Freda Lai <fredalai00@gmail.com>
2024-05-30 09:46:45 +0800

    feat(wren-ui/e2e): add model CRUD and relationship CRUD test for NBA

--- wren-ui/e2e/specs/connectSampleNBA.spec.ts
+++ wren-ui/e2e/specs/connectSampleNBA.spec.ts
@@ -1,6 +1,7 @@
 import { test, expect } from '@playwright/test';
 import * as helper from '../helper';
 import * as homeHelper from '../commonTests/home';
+import * as modelingHelper from '../commonTests/modeling';
 
 const suggestedQuestions = [
   'How many three-pointers were made by each player in each game?',
@@ -23,6 +24,8 @@ test.describe('Test NBA sample dataset', () => {
     }
   });
 
+  test('Check should be in Synced status', modelingHelper.checkDeploySynced);
+
   test('Use suggestion question', async ({ page, baseURL }) => {
     // select first suggested question
     await homeHelper.askSuggestionQuestionTest({
@@ -37,6 +40,42 @@ test.describe('Test NBA sample dataset', () => {
       page,
       baseURL,
       question: 'Which player has made the most three-pointers?',
+    });
+  });
+
+  test('Model CRUD successfully', async ({ page }) => {
+    await modelingHelper.executeModelCRUD(page, {
+      modelDisplayName: 'player',
+      primaryKeyColumn: 'Id',
+    });
+  });
+
+  test('Add relationship successfully', async ({ page }) => {
+    await page.goto('/modeling');
+    await expect(page).toHaveURL('/modeling', { timeout: 60000 });
+
+    // Following the previous test, we assume the customers model is created, and it's have not any relationships
+    await modelingHelper.addRelationship(page, {
+      fromFieldModelDisplayName: 'player',
+      fromFieldColumnDisplayName: 'TeamId',
+      toFieldModelDisplayName: 'team',
+      toFieldColumnDisplayName: 'Id',
+      relationshipType: 'One-to-one',
+    });
+  });
+
+  test(
+    'Check deploy is Undeployed changes',
+    modelingHelper.checkDeployUndeployedChanges,
+  );
+
+  test('Relationship CRUD successfully', async ({ page }) => {
+    await modelingHelper.executeRelationshipCRUD(page, {
+      fromFieldModelDisplayName: 'player',
+      fromFieldColumnDisplayName: 'TeamId',
+      toFieldModelDisplayName: 'team',
+      toFieldColumnDisplayName: 'Id',
+      relationshipType: 'One-to-one',
     });
   });
 });
