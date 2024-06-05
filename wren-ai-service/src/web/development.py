1b1858db0f2269e9d578dc455fc3193d61062fd7
ChihYu Yeh <chihyu.jimmy.yeh@gmail.com>
2024-06-06 03:10:45 +0800

    update dummy api

--- wren-ai-service/src/web/development.py
+++ wren-ai-service/src/web/development.py
@@ -1,9 +1,23 @@
 import asyncio
+import os
+import random
 import time
 
 from fastapi import APIRouter
+from redislite import StrictRedis
 
 router = APIRouter()
+
+REDIS_DB = (
+    StrictRedis(
+        host=os.getenv("REDIS_HOST", "redis"),
+        port=int(os.getenv("REDIS_PORT", 6379)),
+    )
+    if int(os.getenv("WORKERS", 1)) > 1
+    else StrictRedis(
+        "./redis.db",
+    )
+)
 
 
 @router.get("/dummy")
@@ -11,10 +25,12 @@ async def dummy(sleep: int = 4, is_async: bool = True, should_sleep: bool = True
     """
     Dummy endpoint to test async behavior by sleeping for several seconds
     """
+    REDIS_DB.hset("dummy", "dummy", random.randint(1, 10))
+
     if should_sleep:
         if is_async:
             await asyncio.sleep(sleep)
         else:
             time.sleep(sleep)
 
-    return {"status": "dummy"}
+    return {"dummy": REDIS_DB.hget("dummy", "dummy")}
