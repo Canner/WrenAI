c6e773db581dad6cab5d5d57f62be8ea2eb2e0b3
ChihYu Yeh <chihyu.jimmy.yeh@gmail.com>
2024-06-25 14:20:10 +0800

    fix conflict

--- wren-ai-service/tests/pytest/providers/test_loader.py
+++ wren-ai-service/tests/pytest/providers/test_loader.py
@@ -9,15 +9,21 @@ def test_import_mods():
 def test_get_provider():
     loader.import_mods("src.providers")
 
+    # llm provider
     provider = loader.get_provider("openai")
     assert provider.__name__ == "OpenAILLMProvider"
-
-    provider = loader.get_provider("qdrant")
-    assert provider.__name__ == "QdrantProvider"
 
     provider = loader.get_provider("azure_openai")
     assert provider.__name__ == "AzureOpenAILLMProvider"
 
+    provider = loader.get_provider("ollama")
+    assert provider.__name__ == "OllamaLLMProvider"
+
+    # document store provider
+    provider = loader.get_provider("qdrant")
+    assert provider.__name__ == "QdrantProvider"
+
+    # engine provider
     provider = loader.get_provider("wren-ui")
     assert provider.__name__ == "WrenUI"
 
