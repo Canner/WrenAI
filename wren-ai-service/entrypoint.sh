b2e5233e18a4f52c7a81fc2fc40af8fefce8f95b
ChihYu Yeh <chihyu.jimmy.yeh@gmail.com>
2024-06-28 11:58:46 +0800

    fix docker file

---
+++ wren-ai-service/entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+
+# Start wren-ai-service in the background
+uvicorn src.__main__:app --host 0.0.0.0 --port $WREN_AI_SERVICE_PORT --loop uvloop --http httptools &
+
+# Wait for the server to be responsive
+echo "Waiting for wren-ai-service to start..."
+while ! nc -z localhost $WREN_AI_SERVICE_PORT; do   
+    sleep 1  # wait for 1 second before check again
+done
+
+echo "wren-ai-service has started."
+
+python src/force_deploy.py
+
+# Bring wren-ai-service to the foreground
+wait
