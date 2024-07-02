c1a8feb976c96ca8e12a70d1908a33a99e2d0790
ChihYu Yeh <chihyu.jimmy.yeh@gmail.com>
2024-07-02 11:35:07 +0800

    refine prompt

--- wren-ai-service/src/pipelines/sql_regeneration/components/prompts.py
+++ wren-ai-service/src/pipelines/sql_regeneration/components/prompts.py
@@ -1,13 +1,48 @@
 sql_regeneration_system_prompt = """
 ### TASK ###
 
-There are two subtasks in this task.
+- Given a list of user corrections, regenerate the corresponding SQL query.
+- For each modified SQL query, update the corresponding SQL summary, CTE name.
+- If subsequent steps are dependent on the corrected step, make sure to update the SQL query, SQL summary and CTE name in subsequent steps if needed.
+- Regenerate the description after correcting all of the steps.
 
-#### Subtask 1: SQL Query and Summary Regeneration for Each Step ####
-Given each step of the SQL query, SQL summary, cte name and a list of user corrections, 
-your job is to regenerate the corresponding SQL query given the user corrections and regenerate the corresponding SQL summary if necessary.
+### INPUT STRUCTURE ###
 
-#### Subtask 2: Description Generation and SQL Query Regeneration considering all steps ####
-Given the original description and the each step of the SQL query, SQL summary, cte name(some of the steps include regenerated SQL queries, SQL summary from the subtask1), 
-your job is to regenerate the description considering all steps and regenerate the SQL query considering if regenerated SQL query would affectes original SQL query in subsequent steps.
+{
+    "description": "<original_description_string>",
+    "steps": [
+        {
+            "summary": "<original_sql_summary_string>",
+            "sql": "<original_sql_string>",
+            "cte_name": "<original_cte_name_string>",
+            "corrections": [
+                {
+                    "before": {
+                        "type": "<filter/selectItems/relation/groupByKeys/sortings>",
+                        "value": "<original_value_string>"
+                    },
+                    "after": {
+                        "type": "<sql_expression/nl_expression>",
+                        "value": "<new_value_string>"
+                    }
+                },...
+            ]
+        },...
+    ]
+}
+
+### OUTPUT STRUCTURE ###
+
+Generate modified results according to the following in JSON format:
+
+{
+    "description": "<modified_description_string>",
+    "steps": [
+        {
+            "summary": "<modified_sql_summary_string>",
+            "sql": "<modified_sql_string>",
+            "cte_name": "<modified_cte_name_string>",
+        },...
+    ]
+}
 """
