7ff675f642af5786efcbec3a090c04a479012086
ChihYu Yeh <chihyu.jimmy.yeh@gmail.com>
2024-07-16 11:36:47 +0800

    refine ui: use sidebar

--- wren-ai-service/demo/app.py
+++ wren-ai-service/demo/app.py
@@ -18,7 +18,7 @@ from utils import (
 )
 
 st.set_page_config(layout="wide")
-st.title("Wren AI AI Service Demo")
+st.title("Wren AI LLM Service Demo")
 
 if "deployment_id" not in st.session_state:
     st.session_state["deployment_id"] = str(uuid.uuid4())
@@ -61,134 +61,129 @@ def onchange_demo_dataset():
 
 
 if __name__ == "__main__":
-    col1, col2 = st.columns([2, 4])
+    with st.sidebar:
+        st.markdown("## Deploy MDL Model")
+        uploaded_file = st.file_uploader(
+            f"Upload an MDL json file, and the file name must be [xxx]_[datasource]_mdl.json, now we support these datasources: {DATA_SOURCES}",
+            type="json",
+        )
+        st.markdown("or")
+        chosen_demo_dataset = st.selectbox(
+            "Select a demo dataset",
+            key="choose_demo_dataset",
+            options=["music", "nba", "ecommerce"],
+            index=0,
+            on_change=onchange_demo_dataset,
+        )
 
-    with col1:
-        with st.expander("Deploy New Model", expanded=True):
-            uploaded_file = st.file_uploader(
-                f"Upload an MDL json file, and the file name must be [xxx]_[datasource]_mdl.json, now we support these datasources: {DATA_SOURCES}",
-                type="json",
+        if uploaded_file is not None:
+            match = re.match(
+                r".+_(" + "|".join(DATA_SOURCES) + r")_mdl\.json$",
+                uploaded_file.name,
             )
-            st.markdown("or")
-            chosen_demo_dataset = st.selectbox(
-                "Select a demo dataset",
-                key="choose_demo_dataset",
-                options=["music", "nba", "ecommerce"],
-                index=0,
-                on_change=onchange_demo_dataset,
+            if not match:
+                st.error(
+                    f"the file name must be [xxx]_[datasource]_mdl.json, now we support these datasources: {DATA_SOURCES}"
+                )
+                st.stop()
+
+            data_source = match.group(1)
+            st.session_state["chosen_dataset"] = uploaded_file.name.split(
+                f"_{data_source}_mdl.json"
+            )[0]
+            st.session_state["dataset_type"] = data_source
+            st.session_state["mdl_json"] = orjson.loads(
+                uploaded_file.getvalue().decode("utf-8")
+            )
+            save_mdl_json_file(uploaded_file.name, st.session_state["mdl_json"])
+        elif (
+            chosen_demo_dataset
+            and st.session_state["chosen_dataset"] == chosen_demo_dataset
+        ):
+            st.session_state["chosen_dataset"] = chosen_demo_dataset
+            st.session_state["dataset_type"] = "duckdb"
+            st.session_state["mdl_json"] = get_mdl_json(chosen_demo_dataset)
+
+        st.markdown("---")
+
+        if st.session_state["mdl_json"]:
+            chosen_models = st.multiselect(
+                "Select data models for AI to generate MDL metadata",
+                [model["name"] for model in st.session_state["mdl_json"]["models"]],
+            )
+            if chosen_models and st.session_state["chosen_models"] != chosen_models:
+                st.session_state["chosen_models"] = chosen_models
+                st.session_state["mdl_json"] = get_mdl_json(
+                    st.session_state["chosen_dataset"]
+                )
+
+            ai_generate_metadata_ok = st.button(
+                "AI Generate MDL Metadata",
+                disabled=not chosen_models,
+            )
+            if ai_generate_metadata_ok:
+                st.session_state["mdl_json"] = get_new_mdl_json(
+                    chosen_models=chosen_models
+                )
+
+            # Display the model using the selected database
+            st.markdown("MDL Model")
+            st.json(
+                body=st.session_state["mdl_json"],
+                expanded=False,
             )
 
-            if uploaded_file is not None:
-                match = re.match(
-                    r".+_(" + "|".join(DATA_SOURCES) + r")_mdl\.json$",
-                    uploaded_file.name,
-                )
-                if not match:
-                    st.error(
-                        f"the file name must be [xxx]_[datasource]_mdl.json, now we support these datasources: {DATA_SOURCES}"
-                    )
-                    st.stop()
+            show_er_diagram(
+                st.session_state["mdl_json"]["models"],
+                st.session_state["mdl_json"]["relationships"],
+            )
 
-                data_source = match.group(1)
-                st.session_state["chosen_dataset"] = uploaded_file.name.split(
-                    f"_{data_source}_mdl.json"
-                )[0]
-                st.session_state["dataset_type"] = data_source
-                st.session_state["mdl_json"] = orjson.loads(
-                    uploaded_file.getvalue().decode("utf-8")
+            deploy_ok = st.button(
+                "Deploy",
+                use_container_width=True,
+                type="primary",
+            )
+            # Semantics preparation
+            if deploy_ok:
+                rerun_wren_engine(
+                    st.session_state["mdl_json"],
+                    st.session_state["dataset_type"],
+                    st.session_state["chosen_dataset"],
                 )
-                save_mdl_json_file(uploaded_file.name, st.session_state["mdl_json"])
-            elif (
-                chosen_demo_dataset
-                and st.session_state["chosen_dataset"] == chosen_demo_dataset
-            ):
-                st.session_state["chosen_dataset"] = chosen_demo_dataset
-                st.session_state["dataset_type"] = "duckdb"
-                st.session_state["mdl_json"] = get_mdl_json(chosen_demo_dataset)
-
-            st.markdown("---")
-
-            if st.session_state["mdl_json"]:
-                chosen_models = st.multiselect(
-                    "Select data models for AI to generate MDL metadata",
-                    [model["name"] for model in st.session_state["mdl_json"]["models"]],
-                )
-                if chosen_models and st.session_state["chosen_models"] != chosen_models:
-                    st.session_state["chosen_models"] = chosen_models
-                    st.session_state["mdl_json"] = get_mdl_json(
-                        st.session_state["chosen_dataset"]
-                    )
-
-                ai_generate_metadata_ok = st.button(
-                    "AI Generate MDL Metadata",
-                    disabled=not chosen_models,
-                )
-                if ai_generate_metadata_ok:
-                    st.session_state["mdl_json"] = get_new_mdl_json(
-                        chosen_models=chosen_models
-                    )
-
-                # Display the model using the selected database
-                st.markdown("MDL Model")
-                st.json(
-                    body=st.session_state["mdl_json"],
-                    expanded=False,
-                )
-
-                show_er_diagram(
-                    st.session_state["mdl_json"]["models"],
-                    st.session_state["mdl_json"]["relationships"],
-                )
-
-                deploy_ok = st.button(
-                    "Deploy the MDL model using the selected database",
-                    type="primary",
-                )
-                # Semantics preparation
-                if deploy_ok:
-                    rerun_wren_engine(
-                        st.session_state["mdl_json"],
-                        st.session_state["dataset_type"],
-                        st.session_state["chosen_dataset"],
-                    )
-                    prepare_semantics(st.session_state["mdl_json"])
+                prepare_semantics(st.session_state["mdl_json"])
 
     query = st.chat_input(
         "Ask a question about the database",
         disabled=st.session_state["semantics_preparation_status"] != "finished",
     )
 
-    with col2:
-        if query:
-            if (
-                st.session_state["asks_results"]
-                and st.session_state["asks_details_result"]
-            ):
-                st.session_state["query_history"] = {
-                    "sql": st.session_state["chosen_query_result"]["sql"],
-                    "summary": st.session_state["chosen_query_result"]["summary"],
-                    "steps": st.session_state["asks_details_result"]["steps"],
-                }
-            else:
-                st.session_state["query_history"] = None
+    if query:
+        if st.session_state["asks_results"] and st.session_state["asks_details_result"]:
+            st.session_state["query_history"] = {
+                "sql": st.session_state["chosen_query_result"]["sql"],
+                "summary": st.session_state["chosen_query_result"]["summary"],
+                "steps": st.session_state["asks_details_result"]["steps"],
+            }
+        else:
+            st.session_state["query_history"] = None
 
-            # reset relevant session_states
-            # st.session_state["query"] = None
-            st.session_state["asks_results"] = None
-            st.session_state["chosen_query_result"] = None
-            st.session_state["asks_details_result"] = None
-            st.session_state["preview_data_button_index"] = None
-            st.session_state["preview_sql"] = None
+        # reset relevant session_states
+        # st.session_state["query"] = None
+        st.session_state["asks_results"] = None
+        st.session_state["chosen_query_result"] = None
+        st.session_state["asks_details_result"] = None
+        st.session_state["preview_data_button_index"] = None
+        st.session_state["preview_sql"] = None
 
-            ask(query, st.session_state["query_history"])
-        if st.session_state["asks_results"]:
-            show_asks_results()
-        if (
-            st.session_state["asks_details_result"]
-            and st.session_state["chosen_query_result"]
-        ):
+        ask(query, st.session_state["query_history"])
+    if st.session_state["asks_results"]:
+        show_asks_results()
+    if (
+        st.session_state["asks_details_result"]
+        and st.session_state["chosen_query_result"]
+    ):
+        show_asks_details_results(st.session_state["query"])
+    elif st.session_state["chosen_query_result"]:
+        ask_details()
+        if st.session_state["asks_details_result"]:
             show_asks_details_results(st.session_state["query"])
-        elif st.session_state["chosen_query_result"]:
-            ask_details()
-            if st.session_state["asks_details_result"]:
-                show_asks_details_results(st.session_state["query"])
