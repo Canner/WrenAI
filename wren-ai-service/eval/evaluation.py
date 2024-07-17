44b38b7c615e756efb3e63472d6c1ff85fecd59a
Pao-Sheng Wang <paooap.oappao@gmail.com>
2024-07-17 12:06:12 +0800

    feat: support average score as a trace

--- wren-ai-service/eval/evaluation.py
+++ wren-ai-service/eval/evaluation.py
@@ -4,7 +4,7 @@ from typing import Tuple
 from deepeval import evaluate
 from deepeval.test_case import LLMTestCase
 from langfuse import Langfuse
-from langfuse.decorators import langfuse_context
+from langfuse.decorators import langfuse_context, observe
 from tomlkit import parse
 
 from eval.metrics.example import ExampleMetric
@@ -27,21 +27,61 @@ def formatter(prediction: dict) -> dict:
     }
 
 
-def score_metrics(test_case, result, langfuse_client) -> None:
-    for metric in result.metrics_metadata:
-        langfuse_client.score(
-            trace_id=test_case.additional_metadata["trace_id"],
-            name=metric.metric,
-            value=metric.score,
-            comment=metric.reason,
-        )
-
-
 def parse_args() -> Tuple[str]:
     parser = argparse.ArgumentParser()
     parser.add_argument("--path", "-P", type=str, help="Path to eval the prediction")
     args = parser.parse_args()
     return f"outputs/predictions/{args.path}"
+
+
+class Evaluator:
+    def __init__(self, metrics: list):
+        self._score_collector = {}
+        self._langfuse = Langfuse()
+        self._metrics = metrics
+
+    def eval(self, meta: dict, predictions: list) -> None:
+        for prediction in predictions:
+            test_case = LLMTestCase(**formatter(prediction))
+            result = evaluate([test_case], self._metrics)[0]
+            self._score_metrics(test_case, result)
+
+        self._average_score(meta)
+
+    def _score_metrics(self, test_case, result) -> None:
+        for metric in result.metrics_metadata:
+            name = metric.metric
+            score = metric.score
+
+            self._langfuse.score(
+                trace_id=test_case.additional_metadata["trace_id"],
+                name=name,
+                value=score,
+                comment=metric.reason,
+                source="eval",
+            )
+
+            if name not in self._score_collector:
+                self._score_collector[name] = []
+
+            self._score_collector[name].append(score)
+
+    def _average_score(self, meta: dict) -> None:
+        @observe(name="Average Score", capture_input=False, capture_output=False)
+        def wrapper():
+            langfuse_context.update_current_trace(
+                session_id=meta["session_id"],
+                user_id=meta["user_id"],
+            )
+
+            for name, scores in self._score_collector.items():
+                langfuse_context.score_current_trace(
+                    name=name,
+                    value=sum(scores) / len(scores),
+                    comment=f"Average score for {name}",
+                )
+
+        wrapper()
 
 
 if __name__ == "__main__":
@@ -52,11 +92,7 @@ if __name__ == "__main__":
     meta = predicted_file["meta"]
     predictions = predicted_file["predictions"]
 
-    langfuse_client = Langfuse()
-    metrics = [ExampleMetric()]
-    for prediction in predictions:
-        test_case = LLMTestCase(**formatter(prediction))
-        result = evaluate([test_case], metrics)[0]
-        score_metrics(test_case, result, langfuse_client)
+    evaluator = Evaluator([ExampleMetric()])
+    evaluator.eval(meta, predictions)
 
     langfuse_context.flush()
