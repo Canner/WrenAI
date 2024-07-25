394c4eac7da1c5d5e137d91f6f0f765f16ba32cf
Freda Lai <fredalai00@gmail.com>
2024-07-25 17:33:04 +0800

    chore(wren-ui): update bootstrap and launcher

--- docker/bootstrap/init.sh
+++ docker/bootstrap/init.sh
@@ -1,18 +1,6 @@
 #!/bin/sh
 # declare a variable from the environment variable: DATA_PATH
 data_path=${DATA_PATH:-"./"}
-# get default password from environment variable, if not exists, then throw error
-if [ -z "$PG_PASSWORD" ]; then
-  echo "PG_PASSWORD is not set"
-  exit 1
-fi
-pg_password=$PG_PASSWORD
-# get default username from environment variable, if not exists, then throw error
-if [ -z "$PG_USERNAME" ]; then
-  echo "PG_USERNAME is not set"
-  exit 1
-fi
-pg_username=$PG_USERNAME
 
 # touch a empty config.properties if not exists
 # put a content into config.properties if not exists
@@ -39,12 +27,4 @@ fi
 if [ ! -f ${data_path}/mdl/sample.json ]; then
   echo "init mdl/sample.json"
   echo "{\"catalog\": \"test_catalog\", \"schema\": \"test_schema\", \"models\": []}" > ${data_path}/mdl/sample.json
-fi
-
-# put a accounts file in data_path if not exists
-# accounts is a file contains the authentication information
-# the format is like: `username password` per line
-if [ ! -f ${data_path}/accounts ]; then
-  echo "init accounts file"
-  echo "${pg_username} ${pg_password}" > ${data_path}/accounts
 fi
