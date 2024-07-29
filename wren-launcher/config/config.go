03b41f6e75624472a9cab0924f1a0a2fe2147543
Andy Yen <38731840+onlyjackfrost@users.noreply.github.com>
2024-07-29 18:30:02 +0800

    Chore(wren-launcher): support passing api key with arg and helper function (#559)
    * support passing api key with arg and helper method
    
    * extends available args when starting launcher
    
    * refactor launch function

--- wren-launcher/config/config.go
+++ wren-launcher/config/config.go
@@ -6,13 +6,31 @@ import (
 
 // private variable within the config package
 var disableTelemetry bool
+var openaiAPIKey string
+var openaiGenerationModel string
+var llmProvider string
 
 // InitFlags initializes the flag
 func InitFlags() {
 	flag.BoolVar(&disableTelemetry, "disable-telemetry", false, "Disable telemetry if set to true")
+	flag.StringVar(&llmProvider, "llm-provider", "", "The LLM provider to use, valid values are: openai, custom")
+	flag.StringVar(&openaiAPIKey, "openai-api-key", "", "The OPENAI API key")
+	flag.StringVar(&openaiGenerationModel, "openai-generation-model", "", "The OPENAI generation model, valid values are: gpt-4o-mini, gpt-4o, gpt-4-turbo, gpt-3.5-turbo")
 }
 
 // IsTelemetryDisabled exposes the state of the telemetry flag
 func IsTelemetryDisabled() bool {
 	return disableTelemetry
+}
+
+func GetOpenaiAPIKey() string {
+	return openaiAPIKey
+}
+
+func GetOpenaiGenerationModel() string {
+	return openaiGenerationModel
+}
+
+func GetLLMProvider() string {
+	return llmProvider
 }
