679e610c2f5f5092aa0ae0e8629b8c549dac7b1d
Chih-Yu Yeh <chihyu.jimmy.yeh@gmail.com>
2024-07-31 16:40:35 +0800

    chore(wren-ai-service): fix entrypoint (#568)
    * fix entrypoint
    
    * add timeout
    
    * fix
    
    * update
    
    * fix

--- wren-ai-service/entrypoint.sh
+++ wren-ai-service/entrypoint.sh
@@ -4,6 +4,12 @@ set -e
 # Start wren-ai-service in the background
 uvicorn src.__main__:app --host 0.0.0.0 --port $WREN_AI_SERVICE_PORT --loop uvloop --http httptools &
 
+# Wait for wren-ui to be responsive
+echo "Waiting for wren-ui to start..."
+while ! nc -z -w 5 wren-ui $WREN_UI_PORT && ! nc -z -w 5 host.docker.internal $WREN_UI_PORT; do   
+    sleep 1  # wait for 1 second before check again
+done
+
 # Wait for the server to be responsive
 echo "Waiting for wren-ai-service to start..."
 while ! nc -z localhost $WREN_AI_SERVICE_PORT; do   
