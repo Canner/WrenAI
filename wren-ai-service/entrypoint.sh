a98b88d9296d83cbe615e9c1aba983ab27046cf2
ChihYu Yeh <chihyu.jimmy.yeh@gmail.com>
2024-08-10 06:00:02 +0800

    adjust

--- wren-ai-service/entrypoint.sh
+++ wren-ai-service/entrypoint.sh
@@ -1,18 +1,24 @@
 #!/bin/bash
 set -e
 
-# Start wren-ai-service in the background
-uvicorn src.__main__:app --host 0.0.0.0 --port $WREN_AI_SERVICE_PORT --loop uvloop --http httptools &
-
-# Wait for wren-ui to be responsive
-echo "Waiting for wren-ui to start..."
-while ! nc -z -w 5 wren-ui $WREN_UI_PORT && ! nc -z -w 5 host.docker.internal $WREN_UI_PORT; do   
+# Wait for qdrant to be responsive
+echo "Waiting for qdrant to start..."
+while ! nc -z -w 5 qdrant 6333; do   
     sleep 1  # wait for 1 second before check again
 done
+
+# Start wren-ai-service in the background
+uvicorn src.__main__:app --host 0.0.0.0 --port $WREN_AI_SERVICE_PORT --loop uvloop --http httptools &
 
 # Wait for the server to be responsive
 echo "Waiting for wren-ai-service to start..."
 while ! nc -z localhost $WREN_AI_SERVICE_PORT; do   
+    sleep 1  # wait for 1 second before check again
+done
+
+# Wait for wren-ui to be responsive
+echo "Waiting for wren-ui to start..."
+while ! nc -z -w 5 wren-ui $WREN_UI_PORT && ! nc -z -w 5 host.docker.internal $WREN_UI_PORT; do   
     sleep 1  # wait for 1 second before check again
 done
 
