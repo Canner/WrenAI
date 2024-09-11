88903995292c232564a082d6364936209aebe69f
ChihYu Yeh <chihyu.jimmy.yeh@gmail.com>
2024-09-11 15:18:51 +0800

    update

--- wren-ai-service/eval/dashboard/app.py
+++ wren-ai-service/eval/dashboard/app.py
@@ -17,7 +17,14 @@ with st.sidebar:
     st.text_input("Secret Key", type="password", key="langfuse_secret_key_input")
     st.text_input("Host", value="https://cloud.langfuse.com", key="langfuse_host_input")
 
-    if st.button("Save"):
+    if st.button(
+        "Save",
+        disabled=(
+            not st.session_state.langfuse_public_key_input
+            or not st.session_state.langfuse_secret_key_input
+            or not st.session_state.langfuse_host_input
+        ),
+    ):
         st.toast("Initializing Langfuse Client...", icon="⏳")
         st.session_state.langfuse_client = init_langfuse_client(
             st.session_state.langfuse_public_key_input,
@@ -27,8 +34,21 @@ with st.sidebar:
 
 
 if st.session_state.langfuse_client:
-    st.write(
-        st.session_state.langfuse_client.fetch_trace(
-            id="52098743-5bef-490e-8f0d-3d311cb4c464"
+    st.markdown("Please enter a session ID or trace ID to fetch.")
+    col1, col2 = st.columns(2)
+    with col1:
+        st.text_input("Session ID", key="session_id_input")
+    with col2:
+        st.text_input("Trace ID", key="trace_id_input")
+
+    if st.session_state.session_id_input:
+        sessions = st.session_state.langfuse_client.fetch_traces(
+            session_id=st.session_state.session_id_input
         )
-    )
+        st.json(sessions.data)
+
+    if st.session_state.trace_id_input:
+        trace = st.session_state.langfuse_client.fetch_trace(
+            id=st.session_state.trace_id_input
+        )
+        st.json(trace.data.json())
