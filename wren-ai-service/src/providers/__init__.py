03ae0df09c423a7953af17769d4b631eccd9f64b
Pao-Sheng Wang <paooap.oappao@gmail.com>
2024-09-24 17:58:53 +0800

    feat: process diff kind of entry from yaml file

--- wren-ai-service/src/providers/__init__.py
+++ wren-ai-service/src/providers/__init__.py
@@ -1,5 +1,8 @@
 import logging
 
+import yaml
+from yaml.loader import SafeLoader
+
 from src.core.engine import Engine
 from src.core.pipeline import PipelineComponent
 from src.core.provider import DocumentStoreProvider, EmbedderProvider, LLMProvider
@@ -15,53 +18,107 @@ def provider_factory(
     return loader.get_provider(type)(**config)
 
 
-def mock_config():
+def load_config(path: str = "config.yaml") -> list[dict]:
+    with open(path, "r") as f:
+        return list(yaml.load_all(f, Loader=SafeLoader))
+
+
+def process_llm(entry: dict) -> dict:
+    others = {
+        k: v
+        for k, v in entry.items()
+        if k not in ["type", "vender", "api_key", "models"]
+    }
+    returned = {}
+    for model in entry["models"]:
+        model_name = f"{entry['vender']}.{model['model']}"
+        returned[model_name] = {
+            # todo: might be need to put type here
+            "api_key": entry["api_key"],
+            "kwargs": model["kwargs"],
+            **others,
+        }
+    return returned
+
+
+def process_embedder(entry) -> dict:
+    others = {
+        k: v
+        for k, v in entry.items()
+        if k not in ["type", "vender", "api_key", "models"]
+    }
+    returned = {}
+    for model in entry["models"]:
+        model_name = f"{entry['vender']}.{model['model']}"
+        returned[model_name] = {
+            "api_key": entry["api_key"],
+            "dimension": model["dimension"],
+            **others,
+        }
+
+    return returned
+
+
+def process_document_store(entry: dict) -> dict:
     return {
-        "embedder": {
-            "openai.demo-embedding-model": {
-                "dimension": 3072,
-                "api_key": "sk-xxx",
-                "api_base": "https://api.openai.com/v1",
-            }
-        },
-        "llm": {
-            "openai.gpt-4o-mini": {
-                "api_key": "sk-xxx",
-                "kwargs": {"temperature": 0.5},
-            },
-            "azure.gpt-4o-mini": {
-                "api_key": "sk-xxx",
-                "api_base": "https://api.openai.com/v1",
-                "kwargs": {"temperature": 0.5},
-                "version": "2020-05-01",
-            },
-        },
-        "document_store": {
-            "qdrant": {
-                "host": "http://localhost:6333",
-                "api_key": None,
-            }
-        },
-    }, {
-        "indexing": {
-            "embedder": "openai.demo-embedding-model",
-            "document_store": "qdrant",
-        },
-        "sql_generation": {
-            "llm": "openai.gpt-4o-mini",
-        },
+        entry["vender"]: {k: v for k, v in entry.items() if k not in ["type", "vender"]}
     }
 
 
+def process_engine(entry: dict) -> dict:
+    return {engine["name"]: engine for engine in entry["engines"]}
+
+
+def process_pipeline(entry: dict) -> dict:
+    return {
+        pipe["name"]: {
+            "llm": pipe.get("llm_provider"),
+            "embedder": pipe.get("embedding_provider"),
+            "document_store": pipe.get("document_store"),
+            "engine": pipe.get("engine"),
+        }
+        for pipe in entry["pipes"]
+    }
+
+
+def convert_data(config: dict) -> dict:
+    returned = {
+        "embedder": {},
+        "llm": {},
+        "document_store": {},
+        "engine": {},
+        "pipelines": {},
+    }
+
+    type_to_processor = {
+        "llm": process_llm,
+        "embedder": process_embedder,
+        "document_store": process_document_store,
+        "engine": process_engine,
+        "pipeline": process_pipeline,
+    }
+
+    for entry in config:
+        type = entry["type"]
+        processor = type_to_processor.get(type)
+        if not processor:
+            logger.error(f"Unknown type: {type}")
+            raise ValueError(f"Unknown type: {type}")
+
+        converted = processor(entry)
+        returned[type].update(converted)
+
+    return returned
+
+
 def generate_components() -> dict[str, PipelineComponent]:
-    # todo: read from yaml file, this is just an example
-    provider_configs, pipelines_conifg = mock_config()
+    config = convert_data(load_config())
 
     providers = {
-        "embedder": provider_configs.get("embedder", {}),
-        "llm": provider_configs.get("llm", {}),
-        "document_store": provider_configs.get("document_store", {}),
-        "engine": provider_configs.get("engine", {}),
+        "embedder": config.get("embedder", {}),
+        "llm": config.get("llm", {}),
+        "document_store": config.get("document_store", {}),
+        "engine": config.get("engine", {}),
     }
 
     instantiated_providers = {
@@ -83,6 +140,5 @@ def generate_components() -> dict[str, PipelineComponent]:
         )
 
     return {
-        pipe_name: componentize(components)
-        for pipe_name, components in pipelines_conifg.items()
+        pipe_name: componentize(components) for pipe_name, components in config.items()
     }
