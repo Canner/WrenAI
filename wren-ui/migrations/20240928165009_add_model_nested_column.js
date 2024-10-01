da386c0c01b8f0ff821da4c41be1a292e40cb42d
andreashimin <andreashimin@gmail.com>
2024-10-01 23:06:51 +0800

    feat(wren-ui): add migration file to create model_nested_column table

---
+++ wren-ui/migrations/20240928165009_add_model_nested_column.js
@@ -0,0 +1,44 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.createTable('model_nested_column', (table) => {
+    table.increments('id').comment('ID');
+    table.integer('model_id').comment('Reference to model ID');
+    table
+      .string('column_path')
+      .comment(
+        'The path of the nested column, array of strings, [sourceColumnName..sourceColumnName(n)]',
+      );
+
+    table.string('display_name').comment('Display name of the nested column');
+    table
+      .string('source_column_name')
+      .comment('the nested column name in the datasource');
+    table
+      .string('reference_name')
+      .comment('The name used in the MDL structure and query');
+    table
+      .string('type')
+      .comment('Data type, refer to the nested column type in the datasource')
+      .nullable();
+    table
+      .text('properties')
+      .comment(
+        'nested column properties, a json string, the description should be stored here',
+      )
+      .nullable();
+
+    table.foreign('model_id').references('model.id').onDelete('CASCADE');
+    table.timestamps(true, true);
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.dropTable('model_nested_column');
+};
