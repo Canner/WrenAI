e273653e4ae1f736d6aea8150f0db61fc557d320
berzi <anon>
2024-10-10 22:13:43 +0530

    Refactor: /ask/* endpoints

--- wren-ai-service/src/web/v1/routers/__init__.py
+++ wren-ai-service/src/web/v1/routers/__init__.py
@@ -9,15 +9,8 @@ from src.globals import (
     get_service_container,
     get_service_metadata,
 )
-from src.web.v1.routers import relationship_recommendation, semantics_description
-from src.web.v1.services.ask import (
-    AskRequest,
-    AskResponse,
-    AskResultRequest,
-    AskResultResponse,
-    StopAskRequest,
-    StopAskResponse,
-)
+from src.web.v1.routers import relationship_recommendation, semantics_description, asking
+
 from src.web.v1.services.ask_details import (
     AskDetailsRequest,
     AskDetailsResponse,
@@ -60,6 +53,9 @@ from src.web.v1.services.sql_regeneration import (
 router = APIRouter()
 router.include_router(semantics_description.router)
 router.include_router(relationship_recommendation.router)
+router.include_router(asking.router)
+#connected subrouter
+
 
 
 @router.post("/semantics-preparations")
@@ -90,52 +86,6 @@ async def get_prepare_semantics_status(
 ) -> SemanticsPreparationStatusResponse:
     return service_container.semantics_preparation_service.get_prepare_semantics_status(
         SemanticsPreparationStatusRequest(mdl_hash=mdl_hash)
-    )
-
-
-@router.post("/asks")
-async def ask(
-    ask_request: AskRequest,
-    background_tasks: BackgroundTasks,
-    service_container: ServiceContainer = Depends(get_service_container),
-    service_metadata: ServiceMetadata = Depends(get_service_metadata),
-) -> AskResponse:
-    query_id = str(uuid.uuid4())
-    ask_request.query_id = query_id
-    service_container.ask_service._ask_results[query_id] = AskResultResponse(
-        status="understanding",
-    )
-
-    background_tasks.add_task(
-        service_container.ask_service.ask,
-        ask_request,
-        service_metadata=asdict(service_metadata),
-    )
-    return AskResponse(query_id=query_id)
-
-
-@router.patch("/asks/{query_id}")
-async def stop_ask(
-    query_id: str,
-    stop_ask_request: StopAskRequest,
-    background_tasks: BackgroundTasks,
-    service_container: ServiceContainer = Depends(get_service_container),
-) -> StopAskResponse:
-    stop_ask_request.query_id = query_id
-    background_tasks.add_task(
-        service_container.ask_service.stop_ask,
-        stop_ask_request,
-    )
-    return StopAskResponse(query_id=query_id)
-
-
-@router.get("/asks/{query_id}/result")
-async def get_ask_result(
-    query_id: str,
-    service_container: ServiceContainer = Depends(get_service_container),
-) -> AskResultResponse:
-    return service_container.ask_service.get_ask_result(
-        AskResultRequest(query_id=query_id)
     )
 
 
