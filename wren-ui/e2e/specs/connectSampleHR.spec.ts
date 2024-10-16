742d53966a29cb0fce05beaedd3399688f9dd56c
Saai Syvendra <157691467+saai-syvendra@users.noreply.github.com>
2024-10-16 13:59:01 +0000

    feat(wren-ui/e2e):Added E2E tests for HR dataset

---
+++ wren-ui/e2e/specs/connectSampleHR.spec.ts
@@ -0,0 +1,29 @@
+import { test, expect } from '@playwright/test';
+import * as helper from '../helper';
+import * as homeHelper from '../commonTests/home';
+import { sampleDatasets } from '@/apollo/server/data';
+
+const suggestedQuestions = sampleDatasets.hr.questions;
+
+test.describe('Test HR sample dataset', () => {
+  test.beforeAll(async () => {
+    await helper.resetDatabase();
+  });
+
+  test('Select HR dataset and check suggested questions', async ({ page }) => {
+    await page.goto('/setup/connection');
+    await page.getByRole('button', { name: 'HR' }).click();
+    await expect(page).toHaveURL('/home', { timeout: 60000 });
+    for (const suggestedQuestion of suggestedQuestions) {
+      await expect(page.getByText(suggestedQuestion.question)).toBeVisible();
+    }
+  });
+
+  test('Ask first suggested question', async ({ page, baseURL }) => {
+    await homeHelper.askSuggestionQuestionTest({
+      page,
+      baseURL,
+      suggestedQuestion: suggestedQuestions[1].question,
+    });
+  });
+});
