cc41af30c5cd9c389bcc0ab826d207e688e35db7
ChihYu Yeh <chihyu.jimmy.yeh@gmail.com>
2024-10-17 14:45:45 +0800

    fix f401 issue

--- wren-ai-service/eval/metrics/__init__.py
+++ wren-ai-service/eval/metrics/__init__.py
@@ -1,8 +1,19 @@
-from .accuracy import AccuracyMetric  # noqa: F401
-from .answer_relevancy import AnswerRelevancyMetric  # noqa: F401
-from .context_precision import ContextualPrecisionMetric  # noqa: F401
-from .context_recall import ContextualRecallMetric  # noqa: F401
-from .context_relevancy import ContextualRelevancyMetric  # noqa: F401
-from .faithfulness import FaithfulnessMetric  # noqa: F401
-from .spider.exact_match import ExactMatchAccuracy  # noqa: F401
-from .spider.exec_match import ExecutionAccuracy  # noqa: F401
+from .accuracy import AccuracyMetric
+from .answer_relevancy import AnswerRelevancyMetric
+from .context_precision import ContextualPrecisionMetric
+from .context_recall import ContextualRecallMetric
+from .context_relevancy import ContextualRelevancyMetric
+from .faithfulness import FaithfulnessMetric
+from .spider.exact_match import ExactMatchAccuracy
+from .spider.exec_match import ExecutionAccuracy
+
+__all__ = [
+    "AccuracyMetric",
+    "AnswerRelevancyMetric",
+    "ContextualPrecisionMetric",
+    "ContextualRecallMetric",
+    "ContextualRelevancyMetric",
+    "FaithfulnessMetric",
+    "ExactMatchAccuracy",
+    "ExecutionAccuracy",
+]
