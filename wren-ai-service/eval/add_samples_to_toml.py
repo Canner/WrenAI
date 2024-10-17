5152832f6710559faf381e5d55f9df6195ec39b8
Aster Sun <imastr114@gmail.com>
2024-10-17 19:19:48 +0800

    update add_samples_to_toml

--- wren-ai-service/eval/add_samples_to_toml.py
+++ wren-ai-service/eval/add_samples_to_toml.py
@@ -1,19 +1,39 @@
-# read toml
-# utilize utils.get_next_few_items_circular, put n samples in the eval dataset
-"""
-candidate_eval_dataset.append(
-                    {
-                        "categories": [],
-                        "question": ground_truth["question"],
-                        "sql": ground_truth["sql"],
-                        "context": context,
-                        "document": get_documents_given_contexts(
-                            [context], values["mdl"]
-                        ),
-                        "samples": get_next_few_items_circular(
-                            values["ground_truth"], i
-                        ),
-                    }
-                )
-"""
-# write toml
+import argparse
+
+import tomlkit
+
+from eval.utils import (
+    get_next_few_items_circular,
+)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--toml", type=str, help="The toml file name", required=True)
+    args = parser.parse_args()
+
+    if args.toml:
+        # read toml
+        with open(f"eval/dataset/{args.toml}", "r") as f:
+            doc = tomlkit.parse(f.read())
+
+        # get the list of question-sql pairs for generating sample values
+        ground_truth_list = []
+
+        for element in doc["eval_dataset"]:
+            ground_truth_list.append(
+                {"question": element["question"], "sql": element["sql"]}
+            )
+
+        # utilize utils.get_next_few_items_circular, put n samples in the eval dataset
+        new_dataset = []
+
+        for i, element in enumerate(doc["eval_dataset"]):
+            samples = get_next_few_items_circular(ground_truth_list, i)
+            element["samples"] = samples
+            new_dataset.append(element)
+
+        # write toml
+        doc["eval_dataset"] = new_dataset
+
+        with open(f"eval/dataset/added_samples_{args.toml}", "w") as f:
+            f.write(tomlkit.dumps(doc, sort_keys=True))
