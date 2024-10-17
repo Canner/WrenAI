9bf10534f622d3448d42c788bd772e5470de817f
ChihYu Yeh <chihyu.jimmy.yeh@gmail.com>
2024-10-17 14:44:01 +0800

    fix

---
+++ wren-ai-service/eval/metrics/__init__.py
@@ -0,0 +1,8 @@
+from .accuracy import AccuracyMetric  # noqa: F401
+from .answer_relevancy import AnswerRelevancyMetric  # noqa: F401
+from .context_precision import ContextualPrecisionMetric  # noqa: F401
+from .context_recall import ContextualRecallMetric  # noqa: F401
+from .context_relevancy import ContextualRelevancyMetric  # noqa: F401
+from .faithfulness import FaithfulnessMetric  # noqa: F401
+from .spider.exact_match import ExactMatchAccuracy  # noqa: F401
+from .spider.exec_match import ExecutionAccuracy  # noqa: F401
