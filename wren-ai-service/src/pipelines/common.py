b15e5e6c9ccb01347edb4cc52ab65a74eaff0307
ChihYu Yeh <chihyu.jimmy.yeh@gmail.com>
2024-11-13 22:05:50 +0800

    update prompt

--- wren-ai-service/src/pipelines/common.py
+++ wren-ai-service/src/pipelines/common.py
@@ -243,6 +243,7 @@ TEXT_TO_SQL_RULES = """
     SQL
     SELECT _orders.ApprovedTimestamp AS _timestamp FROM orders AS _orders;
 - DON'T USE '.' in column/table alias, replace '.' with '_' in column/table alias.
+- DON'T USE "FILTER(WHERE <condition>)" clause in the query.
 - ONLY USE the following SQL functions if you need to when generating answers:
   - Aggregation functions:
     - AVG
