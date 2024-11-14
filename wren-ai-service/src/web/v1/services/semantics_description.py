72a9c538545853f02c84abc3eaeb48333502a249
Pao Sheng <paooap.oappao@gmail.com>
2024-11-14 11:52:57 +0800

    feat(wren-ai-service): Language Support for Semantics Description Generation (#905)

--- wren-ai-service/src/web/v1/services/semantics_description.py
+++ wren-ai-service/src/web/v1/services/semantics_description.py
@@ -9,7 +9,7 @@ from pydantic import BaseModel
 
 from src.core.pipeline import BasicPipeline
 from src.utils import trace_metadata
-from src.web.v1.services import MetadataTraceable
+from src.web.v1.services import Configuration, MetadataTraceable
 
 logger = logging.getLogger("wren-ai-service")
 
@@ -20,6 +20,7 @@ class SemanticsDescription:
         selected_models: list[str]
         user_prompt: str
         mdl: str
+        configuration: Optional[Configuration] = Configuration()
 
     class Resource(BaseModel, MetadataTraceable):
         class Error(BaseModel):
@@ -61,6 +62,7 @@ class SemanticsDescription:
         template = {
             "user_prompt": request.user_prompt,
             "mdl": mdl_dict,
+            "language": request.configuration.language,
         }
         return [
             {
