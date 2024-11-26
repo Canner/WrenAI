a17ee4a0daec62df2cd2191d9a3ec8ef594b6b29
ChihYu Yeh <chihyu.jimmy.yeh@gmail.com>
2024-11-26 16:31:06 +0800

    update

--- wren-ai-service/src/pipelines/indexing/sql_pairs_preparation.py
+++ wren-ai-service/src/pipelines/indexing/sql_pairs_preparation.py
@@ -1,27 +1,55 @@
+import asyncio
 import logging
 import sys
 import uuid
 from pathlib import Path
 from typing import Any, Dict, List, Optional
 
+import orjson
 from hamilton import base
 from hamilton.async_driver import AsyncDriver
 from haystack import Document, component
+from haystack.components.builders.prompt_builder import PromptBuilder
 from haystack.document_stores.types import DuplicatePolicy
 from langfuse.decorators import observe
+from pydantic import BaseModel
 
 from src.core.pipeline import BasicPipeline
-from src.core.provider import DocumentStoreProvider, EmbedderProvider
+from src.core.provider import DocumentStoreProvider, EmbedderProvider, LLMProvider
 from src.pipelines.indexing import AsyncDocumentWriter
 from src.web.v1.services.sql_pairs_preparation import SqlPair
 
 logger = logging.getLogger("wren-ai-service")
 
 
+sql_intention_generation_system_prompt = """
+### TASK ###
+
+You are a data analyst great at generating the concise and readable summary of a SQL query.
+
+### INSTRUCTIONS ###
+
+- Summary should be concise and readable.
+- Summary should be no longer than 20 words.
+- Don't rephrase keywords in the SQL query, just use them as they are.
+
+### OUTPUT ###
+
+You need to output a JSON object as following:
+{
+    "intention": "<CONCISE_AND_READABLE_SUMMARY_STRING>"
+}
+"""
+
+sql_intention_generation_user_prompt_template = """
+{sql}
+"""
+
+
 @component
 class SqlPairsDescriptionConverter:
     @component.output_types(documents=List[Document])
-    def run(self, sql_pairs: List[SqlPair], id: Optional[str] = None):
+    def run(self, sql_pairs: List[Dict[str, Any]], id: Optional[str] = None):
         logger.info("Converting SQL pairs to documents...")
 
         return {
@@ -29,11 +57,18 @@ class SqlPairsDescriptionConverter:
                 Document(
                     id=str(uuid.uuid4()),
                     meta=(
-                        {"sql_pair_id": sql_pair.id, "project_id": id}
+                        {
+                            "sql_pair_id": sql_pair.get("id"),
+                            "project_id": id,
+                            "sql": sql_pair.get("sql"),
+                        }
                         if id
-                        else {"sql_pair_id": sql_pair.id}
+                        else {
+                            "sql_pair_id": sql_pair.get("id"),
+                            "sql": sql_pair.get("sql"),
+                        }
                     ),
-                    content=sql_pair.sql,
+                    content=sql_pair.get("intention"),
                 )
                 for sql_pair in sql_pairs
             ]
@@ -42,8 +77,45 @@ class SqlPairsDescriptionConverter:
 
 ## Start of Pipeline
 @observe(capture_input=False)
-def convert_sql_pairs_to_documents(
+def prompts(
     sql_pairs: List[SqlPair],
+    prompt_builder: PromptBuilder,
+) -> List[dict]:
+    return [prompt_builder.run(sql=sql_pair.sql) for sql_pair in sql_pairs]
+
+
+@observe(as_type="generation", capture_input=False)
+async def generate_sql_intention(
+    sql_pairs: List[SqlPair],
+    prompts: List[dict],
+    sql_intention_generator: Any,
+) -> List[dict]:
+    async def _task(prompt: str, generator: Any):
+        return await generator.run(prompt=prompt.get("prompt"))
+
+    tasks = [_task(prompt, sql_intention_generator) for prompt in prompts]
+    return await asyncio.gather(*tasks)
+
+
+@observe()
+def post_process(
+    generate_sql_intention: List[dict],
+    sql_pairs: List[SqlPair],
+) -> List[Dict[str, Any]]:
+    intentions = [
+        orjson.loads(result["replies"][0])["intention"]
+        for result in generate_sql_intention
+    ]
+
+    return [
+        {"id": sql_pair.get("id"), "sql": sql_pair.get("sql"), "intention": intention}
+        for sql_pair, intention in zip(sql_pairs, intentions)
+    ]
+
+
+@observe(capture_input=False)
+def convert_sql_pairs_to_documents(
+    sql_pairs: List[Dict[str, Any]],
     sql_pairs_description_converter: SqlPairsDescriptionConverter,
     id: Optional[str] = None,
 ) -> Dict[str, Any]:
@@ -69,18 +141,39 @@ async def write_sql_pairs(
 
 
 ## End of Pipeline
+class SqlIntentionGenerationResult(BaseModel):
+    intention: str
+
+
+SQL_INTENTION_GENERATION_MODEL_KWARGS = {
+    "response_format": {
+        "type": "json_schema",
+        "json_schema": {
+            "name": "matched_schema",
+            "schema": SqlIntentionGenerationResult.model_json_schema(),
+        },
+    }
+}
 
 
 class SqlPairsPreparation(BasicPipeline):
     def __init__(
         self,
         embedder_provider: EmbedderProvider,
+        llm_provider: LLMProvider,
         document_store_provider: DocumentStoreProvider,
         **kwargs,
     ) -> None:
         sql_pairs_store = document_store_provider.get_store(dataset_name="sql_pairs")
 
         self._components = {
+            "prompt_builder": PromptBuilder(
+                template=sql_intention_generation_user_prompt_template
+            ),
+            "sql_intention_generator": llm_provider.get_generator(
+                system_prompt=sql_intention_generation_system_prompt,
+                generation_kwargs=SQL_INTENTION_GENERATION_MODEL_KWARGS,
+            ),
             "document_embedder": embedder_provider.get_document_embedder(),
             "sql_pairs_description_converter": SqlPairsDescriptionConverter(),
             "sql_pairs_writer": AsyncDocumentWriter(
--- wren-ai-service/src/web/v1/routers/__init__.py
+++ wren-ai-service/src/web/v1/routers/__init__.py
@@ -6,7 +6,7 @@ from src.web.v1.routers import (
     question_recommendation,
     relationship_recommendation,
     semantics_description,
-    semantics_preparations,
+    semantics_preparation,
     sql_answers,
     sql_expansions,
     sql_explanations,
@@ -20,7 +20,7 @@ router.include_router(ask_details.router)
 router.include_router(question_recommendation.router)
 router.include_router(relationship_recommendation.router)
 router.include_router(semantics_description.router)
-router.include_router(semantics_preparations.router)
+router.include_router(semantics_preparation.router)
 router.include_router(sql_answers.router)
 router.include_router(sql_expansions.router)
 router.include_router(sql_explanations.router)
