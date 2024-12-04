39f4f1ca376d6a3006c8dd8c3b9f813d4ee3961b
ChihYu Yeh <chihyu.jimmy.yeh@gmail.com>
2024-12-04 13:57:06 +0800

    update

--- wren-ai-service/tests/pytest/apis/__init__.py
+++ wren-ai-service/tests/pytest/apis/__init__.py
@@ -1,13 +1,19 @@
+import json
 import os
 import uuid
 
+import orjson
 import pytest
+from fastapi.testclient import TestClient
 
 
 @pytest.fixture(scope="module", autouse=True)
 def app():
     os.environ["CONFIG_PATH"] = "tests/data/config.test.yaml"
     from src.__main__ import app
+
+    # make sure semantics preparation is ready per test
+    semantics_preparation(GLOBAL_DATA["semantics_preperation_id"], app)
 
     yield app
     # Clean up (if necessary)
@@ -18,3 +24,25 @@ GLOBAL_DATA = {
     "semantics_preperation_id": str(uuid.uuid4()),
     "query_id": None,
 }
+
+
+def semantics_preparation(semantics_preperation_id: str, app: app):
+    with TestClient(app) as client:
+        with open("tests/data/book_2_mdl.json", "r") as f:
+            mdl_str = orjson.dumps(json.load(f)).decode("utf-8")
+
+        response = client.post(
+            url="/v1/semantics-preparations",
+            json={
+                "mdl": mdl_str,
+                "id": semantics_preperation_id,
+            },
+        )
+
+        status = "indexing"
+
+        while status != "finished":
+            response = client.get(
+                url=f"/v1/semantics-preparations/{semantics_preperation_id}/status"
+            )
+            status = response.json()["status"]
--- wren-ai-service/tests/pytest/apis/test_ask.py
+++ wren-ai-service/tests/pytest/apis/test_ask.py
@@ -3,7 +3,7 @@ from fastapi.testclient import TestClient
 from tests.pytest.apis import GLOBAL_DATA, app
 
 
-def test_asks_with_successful_query(app: app):
+def test_asks_with_text_to_sql_query(app: app):
     with TestClient(app) as client:
         semantics_preparation_id = GLOBAL_DATA["semantics_preperation_id"]
 
@@ -11,7 +11,7 @@ def test_asks_with_successful_query(app: app):
             url="/v1/asks",
             json={
                 "query": "How many books are there?",
-                "id": semantics_preparation_id,
+                "mdl_hash": semantics_preparation_id,
             },
         )
 
@@ -30,9 +30,68 @@ def test_asks_with_successful_query(app: app):
 
         assert response.status_code == 200
         assert response.json()["status"] == "finished" or "failed"
+        assert response.json()["type"] == "TEXT_TO_SQL"
+
+
+def test_asks_with_general_query(app: app):
+    with TestClient(app) as client:
+        semantics_preparation_id = GLOBAL_DATA["semantics_preperation_id"]
+
+        response = client.post(
+            url="/v1/asks",
+            json={
+                "query": "describe the database schema to me",
+                "mdl_hash": semantics_preparation_id,
+            },
+        )
+
+        assert response.status_code == 200
+        assert response.json()["query_id"] != ""
+
+        query_id = response.json()["query_id"]
+
+        response = client.get(url=f"/v1/asks/{query_id}/result")
+        while (
+            response.json()["status"] != "finished"
+            and response.json()["status"] != "failed"
+        ):
+            response = client.get(url=f"/v1/asks/{query_id}/result")
+
+        assert response.status_code == 200
+        assert response.json()["status"] == "finished" or "failed"
+        assert response.json()["type"] == "GENERAL"
 
 
 def test_asks_with_invalid_query(app: app):
+    with TestClient(app) as client:
+        semantics_preparation_id = GLOBAL_DATA["semantics_preperation_id"]
+
+        response = client.post(
+            url="/v1/asks",
+            json={
+                "query": "how are you",
+                "mdl_hash": semantics_preparation_id,
+            },
+        )
+
+        assert response.status_code == 200
+        assert response.json()["query_id"] != ""
+
+        query_id = response.json()["query_id"]
+
+        response = client.get(url=f"/v1/asks/{query_id}/result")
+        while (
+            response.json()["status"] != "finished"
+            and response.json()["status"] != "failed"
+        ):
+            response = client.get(url=f"/v1/asks/{query_id}/result")
+
+        assert response.status_code == 200
+        assert response.json()["status"] == "finished" or "failed"
+        assert response.json()["type"] == "MISLEADING_QUERY"
+
+
+def test_asks_with_invalid_data_format(app: app):
     with TestClient(app) as client:
         response = client.post(
             url="/v1/asks",
