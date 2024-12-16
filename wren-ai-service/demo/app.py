a87ccca1bf8637997a858a57c1bba49b7ba8557a
Chih-Yu Yeh <chihyu.jimmy.yeh@gmail.com>
2024-12-16 14:09:28 +0800

    feat(wren-ai-service):text2chart (#679)

--- wren-ai-service/demo/app.py
+++ wren-ai-service/demo/app.py
@@ -1,13 +1,19 @@
 import re
 import uuid
 
+import extra_streamlit_components as stx
 import orjson
+import pytz
 import streamlit as st
 from utils import (
     DATA_SOURCES,
     ask,
     ask_details,
+    generate_chart,
+    get_data_from_wren_engine,
     get_mdl_json,
+    get_sql_answer,
+    on_click_adjust_chart,
     prepare_semantics,
     rerun_wren_engine,
     save_mdl_json_file,
@@ -60,6 +66,10 @@ if "sql_regeneration_results" not in st.session_state:
     st.session_state["sql_regeneration_results"] = None
 if "language" not in st.session_state:
     st.session_state["language"] = "English"
+if "timezone" not in st.session_state:
+    st.session_state["timezone"] = "UTC"
+if "chosen_tab_id" not in st.session_state:
+    st.session_state["chosen_tab_id"] = "1"
 
 
 def onchange_demo_dataset():
@@ -68,6 +78,10 @@ def onchange_demo_dataset():
 
 def onchange_language():
     st.session_state["language"] = st.session_state["language_selectbox"]
+
+
+def onchange_timezone():
+    st.session_state["timezone"] = st.session_state["timezone_selectbox"]
 
 
 with st.sidebar:
@@ -86,25 +100,6 @@ with st.sidebar:
         ],
         index=0,
         on_change=onchange_demo_dataset,
-    )
-
-    st.selectbox(
-        "Language",
-        key="language_selectbox",
-        options=[
-            "English",
-            "Spanish",
-            "French",
-            "TraditionalChinese",
-            "SimplifiedChinese",
-            "German",
-            "Portuguese",
-            "Russian",
-            "Japanese",
-            "Korean",
-        ],
-        index=0,
-        on_change=onchange_language,
     )
 
     if uploaded_file is not None:
@@ -135,29 +130,56 @@ with st.sidebar:
         st.session_state["dataset_type"] = "duckdb"
         st.session_state["mdl_json"] = get_mdl_json(chosen_demo_dataset)
 
-    st.markdown("---")
-
     if st.session_state["mdl_json"]:
         # Display the model using the selected database
-        st.markdown("MDL Model")
+        st.markdown("MDL Model Preview")
         st.json(
             body=st.session_state["mdl_json"],
             expanded=False,
         )
 
-        deploy_ok = st.button(
-            "Deploy",
-            use_container_width=True,
-            type="primary",
+    st.markdown("## Settings")
+    st.selectbox(
+        "LLM Output Language",
+        key="language_selectbox",
+        options=[
+            "English",
+            "Spanish",
+            "French",
+            "TraditionalChinese",
+            "SimplifiedChinese",
+            "German",
+            "Portuguese",
+            "Russian",
+            "Japanese",
+            "Korean",
+        ],
+        index=0,
+        on_change=onchange_language,
+    )
+    st.selectbox(
+        "User Timezone",
+        key="timezone_selectbox",
+        options=pytz.all_timezones,
+        index=pytz.all_timezones.index(st.session_state["timezone"]),
+        on_change=onchange_timezone,
+    )
+
+    st.markdown("---")
+
+    deploy_ok = st.button(
+        "Deploy",
+        use_container_width=True,
+        type="primary",
+    )
+    # Semantics preparation
+    if deploy_ok:
+        rerun_wren_engine(
+            st.session_state["mdl_json"],
+            st.session_state["dataset_type"],
+            st.session_state["chosen_dataset"],
         )
-        # Semantics preparation
-        if deploy_ok:
-            rerun_wren_engine(
-                st.session_state["mdl_json"],
-                st.session_state["dataset_type"],
-                st.session_state["chosen_dataset"],
-            )
-            prepare_semantics(st.session_state["mdl_json"])
+        prepare_semantics(st.session_state["mdl_json"])
 
 query = st.chat_input(
     "Ask a question about the database",
@@ -181,12 +203,89 @@ if query:
     st.session_state["preview_data_button_index"] = None
     st.session_state["preview_sql"] = None
 
-    ask(query, st.session_state["query_history"])
+    ask(query, st.session_state["timezone"], st.session_state["query_history"])
 if st.session_state["asks_results"]:
     show_asks_results()
-if st.session_state["asks_details_result"] and st.session_state["chosen_query_result"]:
-    show_asks_details_results(st.session_state["query"])
-elif st.session_state["chosen_query_result"]:
-    ask_details()
-    if st.session_state["asks_details_result"]:
-        show_asks_details_results(st.session_state["query"])
+
+    chosen_tab_id = stx.tab_bar(
+        data=[
+            stx.TabBarItemData(id=1, title="Answer", description=""),
+            stx.TabBarItemData(id=2, title="SQL Details", description=""),
+            stx.TabBarItemData(id=3, title="Chart", description=""),
+        ],
+        default=st.session_state["chosen_tab_id"],
+    )
+    st.session_state["chosen_tab_id"] = chosen_tab_id
+
+    if chosen_tab_id == "1":
+        if st.session_state["chosen_query_result"]:
+            st.markdown("### Data Answer")
+            get_sql_answer(
+                st.session_state["chosen_query_result"]["query"],
+                st.session_state["chosen_query_result"]["sql"],
+                st.session_state["dataset_type"],
+                st.session_state["mdl_json"],
+            )
+
+            st.markdown("### Data Preview")
+            st.dataframe(
+                get_data_from_wren_engine(
+                    st.session_state["chosen_query_result"]["sql"],
+                    st.session_state["dataset_type"],
+                    st.session_state["mdl_json"],
+                )
+            )
+    elif chosen_tab_id == "2":
+        if st.session_state["chosen_query_result"]:
+            ask_details_result = ask_details()
+            if ask_details_response := ask_details_result.get("response"):
+                st.session_state["asks_details_result"] = ask_details_response
+                st.session_state["sql_explanation_question"] = None
+                st.session_state["sql_explanation_steps_with_analysis"] = None
+                st.session_state["sql_analysis_results"] = None
+                st.session_state["sql_explanation_results"] = None
+            else:
+                st.error(
+                    f'An error occurred while processing the query: {ask_details_result.get("error")}',
+                    icon="🚨",
+                )
+            if st.session_state["asks_details_result"]:
+                show_asks_details_results()
+    else:
+        if st.session_state["chosen_query_result"]:
+            chart_response = generate_chart(
+                query=st.session_state["chosen_query_result"]["query"],
+                sql=st.session_state["chosen_query_result"]["sql"],
+                language=st.session_state["language"],
+                dataset_type=st.session_state["dataset_type"],
+                manifest=st.session_state["mdl_json"],
+                limit=500,
+            )
+            if chart_result := chart_response.get("response"):
+                if reasoning := chart_result["reasoning"]:
+                    st.markdown("### Reasoning for making this chart")
+                    st.markdown(f"{reasoning}")
+                if vega_lite_schema := chart_result["chart_schema"]:
+                    st.markdown("### Vega-Lite Schema")
+                    st.json(vega_lite_schema, expanded=False)
+                    st.vega_lite_chart(vega_lite_schema, use_container_width=True)
+
+                    st.button(
+                        "Adjust Chart",
+                        on_click=on_click_adjust_chart,
+                        kwargs={
+                            "query": st.session_state["chosen_query_result"]["query"],
+                            "sql": st.session_state["chosen_query_result"]["sql"],
+                            "chart_schema": vega_lite_schema,
+                            "language": st.session_state["language"],
+                            "reasoning": chart_result["reasoning"],
+                            "dataset_type": st.session_state["dataset_type"],
+                            "manifest": st.session_state["mdl_json"],
+                            "limit": 500,
+                        },
+                    )
+            else:
+                st.error(
+                    f'An error occurred while processing the query: {chart_response.get("error")}',
+                    icon="🚨",
+                )
