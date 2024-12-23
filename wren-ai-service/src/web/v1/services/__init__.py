4a1fbb718de0f63fc4d17e68109054fcad7f869d
ChihYu Yeh <chihyu.jimmy.yeh@gmail.com>
2024-12-23 10:15:00 +0800

    update default lang

--- wren-ai-service/src/web/v1/services/__init__.py
+++ wren-ai-service/src/web/v1/services/__init__.py
@@ -41,7 +41,7 @@ class Configuration(BaseModel):
         return f'{current_time.strftime("%Y-%m-%d %A %H:%M:%S")}'  # YYYY-MM-DD weekday_name HH:MM:SS, ex: 2024-10-23 Wednesday 12:00:00
 
     fiscal_year: Optional[FiscalYear] = None
-    language: Optional[str] = "en"
+    language: Optional[str] = "English"
     timezone: Optional[Timezone] = Timezone()
 
 
