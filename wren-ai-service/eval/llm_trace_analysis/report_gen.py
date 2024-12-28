ae24b79de2697e254b0d5145412cda00c34fbdde
ChihYu Yeh <chihyu.jimmy.yeh@gmail.com>
2024-12-28 15:08:00 +0800

    update

--- wren-ai-service/eval/llm_trace_analysis/report_gen.py
+++ wren-ai-service/eval/llm_trace_analysis/report_gen.py
@@ -1,11 +1,12 @@
 import asyncio
+import json
 import os
 from datetime import datetime
-from typing import Optional
+from typing import List, Optional
 
 from dotenv import load_dotenv
 from langfuse.api.client import AsyncFernLangfuse
-from langfuse.client import FetchTracesResponse
+from langfuse.client import FetchTracesResponse, TraceWithDetails
 
 load_dotenv("eval/llm_trace_analysis/.env", override=True)
 
@@ -41,7 +42,7 @@ async def get_all_traces(
     from_timestamp: Optional[datetime] = None,
     to_timestamp: Optional[datetime] = None,
     release: Optional[str] = None,
-):
+) -> List[TraceWithDetails]:
     # Get first page to determine total pages
     first_page = await client.trace.list(
         name=name,
@@ -76,18 +77,109 @@ async def get_all_traces(
     return traces
 
 
+def filter_traces(
+    traces: List[TraceWithDetails], trace_names: List[str]
+) -> List[TraceWithDetails]:
+    return [trace for trace in traces if trace.name in trace_names]
+
+
+def gen_report(
+    traces: List[TraceWithDetails], trace_name: str, only_errors: bool = False
+):
+    def _get_output(trace: TraceWithDetails, trace_name: str):
+        trace_url = os.getenv("LANGFUSE_HOST") + trace.html_path
+
+        match trace_name:
+            case "Prepare Semantics":
+                return trace_url
+            case "Ask Question":
+                return trace_url
+            case "Ask Details(Breakdown SQL)":
+                return trace_url
+            case "SQL Expansion":
+                return trace_url
+            case "SQL Answer":
+                return trace_url
+            case "Generate Semantics Description":
+                return trace_url
+            case "Generate Relationship Recommendation":
+                return trace_url
+            case "Genereate Chart":
+                return trace_url
+            case "Adjust Chart":
+                return trace_url
+            case "Generate Question Recommendation":
+                return trace_url
+            case _:
+                return None
+
+    if only_errors:
+        return [
+            _get_output(trace, trace_name)
+            for trace in traces
+            if json.loads(trace.output).get("metadata", {}).get("error_type", "")
+        ]
+    else:
+        return [_get_output(trace, trace_name) for trace in traces]
+
+
+def save_report(report_by_release: dict):
+    with open(
+        f"outputs/report_{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.json", "w"
+    ) as f:
+        json.dump(report_by_release, f)
+
+
 async def main():
+    RELEASES = ["0.15.3", "0.14.2"]
+    TRACE_NAMES = [
+        # indexing
+        "Prepare Semantics",
+        # text2sql
+        "Ask Question",
+        "Ask Details(Breakdown SQL)",
+        "SQL Expansion",
+        "SQL Answer",
+        # context enrichment
+        "Generate Semantics Description",
+        "Generate Relationship Recommendation",
+        # chart
+        "Genereate Chart",
+        "Adjust Chart",
+        # others
+        "Generate Question Recommendation",
+    ]
+    ONLY_ERRORS = True
+    traces_by_release = {}
+    report_by_release = {}
+
     client = get_langfuse_client()
-    releases = ["0.15.3", "0.14.2"]
 
     # Use asyncio.gather to fetch traces for all releases concurrently
-    traces_by_release = await asyncio.gather(
-        *[get_all_traces(client, release=release) for release in releases]
+    _traces_by_release = await asyncio.gather(
+        *[get_all_traces(client, release=release) for release in RELEASES]
     )
 
     # Print results for each release
-    for release, traces in zip(releases, traces_by_release):
-        print(f"Release {release}: {len(traces)} traces")
+    for release, traces in zip(RELEASES, _traces_by_release):
+        traces_by_release[release] = {
+            trace_name: filter_traces(traces, [trace_name])
+            for trace_name in TRACE_NAMES
+        }
+
+    for release, traces in traces_by_release.items():
+        for trace_name, trace_group in traces.items():
+            print(f"Release: {release}, Trace Name: {trace_name}")
+            if release not in report_by_release:
+                report_by_release[release] = {}
+
+            report_by_release[release][trace_name] = gen_report(
+                trace_group,
+                trace_name,
+                only_errors=ONLY_ERRORS,
+            )
+
+    save_report(report_by_release)
 
 
 if __name__ == "__main__":
