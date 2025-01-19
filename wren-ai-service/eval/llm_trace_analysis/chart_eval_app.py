4dbea74ef174a59e31e703617631e61c52b5535d
ChihYu Yeh <chihyu.jimmy.yeh@gmail.com>
2025-01-19 20:18:48 +0800

    finish chart eval app v1

--- wren-ai-service/eval/llm_trace_analysis/chart_eval_app.py
+++ wren-ai-service/eval/llm_trace_analysis/chart_eval_app.py
@@ -3,46 +3,66 @@ import os
 from typing import Any, Dict, List
 
 import orjson
+import pytz
 import streamlit as st
 from utils import (
     ObservationsView,
     TraceWithDetails,
     get_all_observations,
+    get_all_traces,
     get_langfuse_client,
 )
 
 
-def get_parent_chart_observations(
+def match(
+    traces: List[TraceWithDetails],
     observations: List[ObservationsView],
-    chart_spans: List[TraceWithDetails],
+    spans: List[TraceWithDetails],
 ) -> List[ObservationsView]:
-    filtered_chart_spans = []
-    chart_span_id = 0
+    filtered_traces = []
+    filtered_spans = []
+    _id = 0
     for observation in observations:
-        while observation.parent_observation_id != chart_spans[chart_span_id].id:
-            chart_span_id += 1
-        filtered_chart_spans.append(chart_spans[chart_span_id])
-        chart_span_id += 1
+        while observation.parent_observation_id != spans[_id].id:
+            _id += 1
+        filtered_spans.append(spans[_id])
+        filtered_traces.append(traces[_id])
+        _id += 1
 
-    return filtered_chart_spans
+    return filtered_traces, filtered_spans, observations
 
 
 def get_chart_data(
-    chart_spans: List[TraceWithDetails],
-    observations: List[ObservationsView],
+    chart_traces: List[TraceWithDetails],
+    chart_spans: List[ObservationsView],
+    char_observations: List[ObservationsView],
+    project_id: str = "",
+    chart_types: set[str] = set(),
+    llms: set[str] = set(),
+    skip_empty_chart: bool = False,
 ) -> List[Dict[str, Any]]:
-    filtered_chart_spans = get_parent_chart_observations(
-        observations,
-        chart_spans,
-    )
-
     chart_data = []
-    for chart_span, chart_observation in zip(filtered_chart_spans, observations):
+    tz = pytz.timezone("Asia/Taipei")
+
+    for chart_trace, chart_span, chart_observation in zip(
+        *match(chart_traces, char_observations, chart_spans)
+    ):
         try:
             chart_output = orjson.loads(chart_observation.output["replies"][0])
+
+            if project_id and project_id != str(chart_trace.metadata["project_id"]):
+                continue
+            if chart_types and chart_output.get("chart_type", "") not in chart_types:
+                continue
+            if llms and chart_observation.output["meta"][0]["model"] not in llms:
+                continue
+            if skip_empty_chart and not chart_output.get("chart_schema", ""):
+                continue
+
             chart_data.append(
                 {
-                    "start_time": chart_span.start_time,
+                    "project_id": chart_trace.metadata["project_id"],
+                    "start_time": chart_span.start_time.astimezone(tz),
                     "latency": (
                         chart_span.end_time - chart_span.start_time
                     ).total_seconds(),
@@ -61,19 +81,33 @@ def get_chart_data(
     return chart_data
 
 
+def on_change_project_id():
+    st.session_state["project_id"] = st.session_state["project_id_input"]
+
+
+def on_change_chart_types():
+    st.session_state["chart_types"] = set(st.session_state["chart_types_input"])
+
+
+def on_change_llms():
+    st.session_state["llms"] = set(st.session_state["llms_input"])
+
+
+def on_change_skip_empty_chart():
+    st.session_state["skip_empty_chart"] = st.session_state["skip_empty_chart_input"]
+
+
 def load_more():
     st.session_state["load_num"] += 10
 
 
-async def main():
-    st.set_page_config(layout="wide")
-    st.title("Chart Evaluation")
-
-    if "load_num" not in st.session_state:
-        st.session_state["load_num"] = 10
-
+async def get_chart_traces_spans_and_observations():
     client = get_langfuse_client()
-    generate_chart_spans, generate_chart_observations = await asyncio.gather(
+    chart_traces, chart_spans, chart_observations = await asyncio.gather(
+        get_all_traces(
+            client,
+            name="Generate Chart",
+        ),
         get_all_observations(
             client,
             name="Chart Generation",
@@ -86,9 +120,95 @@ async def main():
         ),
     )
 
+    return chart_traces, chart_spans, chart_observations
+
+
+async def main():
+    st.set_page_config(layout="wide")
+    st.title("Chart Evaluation")
+
+    if "chart_traces" not in st.session_state:
+        st.session_state["chart_traces"] = []
+    if "chart_spans" not in st.session_state:
+        st.session_state["chart_spans"] = []
+    if "chart_observations" not in st.session_state:
+        st.session_state["chart_observations"] = []
+    if "load_num" not in st.session_state:
+        st.session_state["load_num"] = 10
+    if "project_id" not in st.session_state:
+        st.session_state["project_id"] = ""
+    if "chart_types" not in st.session_state:
+        st.session_state["chart_types"] = set()
+    if "llms" not in st.session_state:
+        st.session_state["llms"] = set()
+    if "skip_empty_chart" not in st.session_state:
+        st.session_state["skip_empty_chart"] = False
+
+    if (
+        not st.session_state["chart_traces"]
+        or not st.session_state["chart_spans"]
+        or not st.session_state["chart_observations"]
+    ):
+        (
+            chart_traces,
+            chart_spans,
+            chart_observations,
+        ) = await get_chart_traces_spans_and_observations()
+        st.session_state["chart_traces"] = chart_traces
+        st.session_state["chart_spans"] = chart_spans
+        st.session_state["chart_observations"] = chart_observations
+
+    st.text_input(
+        "Enter project_id",
+        key="project_id_input",
+        value=st.session_state["project_id"],
+        on_change=on_change_project_id,
+    )
+
+    st.multiselect(
+        "Select chart types",
+        options=[
+            "line",
+            "multi_line",
+            "bar",
+            "stacked_bar",
+            "grouped_bar",
+            "pie",
+            "area",
+        ],
+        key="chart_types_input",
+        default=st.session_state["chart_types"],
+        on_change=on_change_chart_types,
+    )
+
+    st.multiselect(
+        "Select LLMs",
+        options=[
+            "gpt-4o",
+            "gpt-4o-2024-08-06",
+            "gpt-4o-mini",
+            "gpt-4o-mini-2024-07-18",
+        ],
+        key="llms_input",
+        default=st.session_state["llms"],
+        on_change=on_change_llms,
+    )
+
+    st.checkbox(
+        "Skip empty chart",
+        key="skip_empty_chart_input",
+        value=st.session_state["skip_empty_chart"],
+        on_change=on_change_skip_empty_chart,
+    )
+
     chart_data = get_chart_data(
-        generate_chart_spans,
-        generate_chart_observations,
+        st.session_state["chart_traces"],
+        st.session_state["chart_spans"],
+        st.session_state["chart_observations"],
+        project_id=st.session_state["project_id"],
+        chart_types=st.session_state["chart_types"],
+        llms=st.session_state["llms"],
+        skip_empty_chart=st.session_state["skip_empty_chart"],
     )
 
     for i, row in enumerate(chart_data[: st.session_state["load_num"]]):
