7aee8d587ae72e3f538c4378a9832562d245d6a3
ChihYu Yeh <chihyu.jimmy.yeh@gmail.com>
2025-01-24 12:21:54 +0800

    update

--- wren-ai-service/src/pipelines/generation/sql_expansion.py
+++ wren-ai-service/src/pipelines/generation/sql_expansion.py
@@ -26,6 +26,7 @@ You are a great data analyst. You are now given a task to expand original SQL fr
 - Columns are given from the user's adjustment request
 - Columns to be adjusted must belong to the given database schema; if no such column exists, keep sql empty string
 - You can add/delete/modify columns, add/delete/modify keywords such as DISTINCT or apply aggregate functions on columns
+- Consider current time from user input if user's adjustment request is related to date and time
 """
 
 sql_expansion_user_prompt_template = """
