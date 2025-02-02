a08ebfabc69bb4cfe5717a88552aef93eb95ad18
ChihYu Yeh <chihyu.jimmy.yeh@gmail.com>
2025-02-02 08:14:35 +0800

    change limit to 50

--- wren-ai-service/demo/run_sql.py
+++ wren-ai-service/demo/run_sql.py
@@ -66,7 +66,7 @@ def main():
                     sql=command,
                     dataset_type=data_source,
                     manifest=mdl_json,
-                    limit=10,
+                    limit=50,
                 )
                 print(f"\nExecution result:\n{df.to_string()}\n")
             except Exception as e:
