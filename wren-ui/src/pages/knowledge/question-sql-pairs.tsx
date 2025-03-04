4f045eb50fbe062668283b8b426dbbb2a2b61965
Freda Lai <fredalai00@gmail.com>
2025-03-04 15:47:26 +0800

    feat(wren-ui): Support header bar and sidebar for Knowledge page

---
+++ wren-ui/src/pages/knowledge/question-sql-pairs.tsx
@@ -0,0 +1,9 @@
+import SiderLayout from '@/components/layouts/SiderLayout';
+
+export default function ManageQuestionSQLPairs() {
+  return (
+    <SiderLayout loading={false}>
+      <div>Knowledge Page</div>
+    </SiderLayout>
+  );
+}
