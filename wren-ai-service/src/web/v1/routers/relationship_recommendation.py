3def38a6a17ad7de0cc62c8d42fbfb6dcb616928
Pao Sheng <paooap.oappao@gmail.com>
2025-03-05 17:36:01 +0800

    feat: trace id in relationship recommendation

--- wren-ai-service/src/web/v1/routers/relationship_recommendation.py
+++ wren-ai-service/src/web/v1/routers/relationship_recommendation.py
@@ -107,7 +107,7 @@ class GetResponse(BaseModel):
     status: Literal["generating", "finished", "failed"]
     response: Optional[dict]
     error: Optional[dict]
-
+    trace_id: Optional[str] = None
 
 @router.get(
     "/relationship-recommendations/{id}",
@@ -124,4 +124,5 @@ async def get(
         status=resource.status,
         response=resource.response,
         error=resource.error and resource.error.model_dump(),
+        trace_id=resource.trace_id,
     )
