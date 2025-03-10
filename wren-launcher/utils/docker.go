e39c806d37d305a246f9fe27b9acb5e1432e6c07
ChihYu Yeh <chihyu.jimmy.yeh@gmail.com>
2025-03-10 16:53:59 +0800

    remove LLM_OPENAI_API_KEY and EMBEDDER_OPENAI_API_KEY

--- wren-launcher/utils/docker.go
+++ wren-launcher/utils/docker.go
@@ -45,13 +45,9 @@ func replaceEnvFileContent(content string, projectDir string, openaiApiKey strin
 	reg = regexp.MustCompile(`SHOULD_FORCE_DEPLOY=(.*)`)
 	str = reg.ReplaceAllString(str, "SHOULD_FORCE_DEPLOY=1")
 
-	// replace LLM_OPENAI_API_KEY
-	reg = regexp.MustCompile(`LLM_OPENAI_API_KEY=(.*)`)
-	str = reg.ReplaceAllString(str, "LLM_OPENAI_API_KEY="+openaiApiKey)
-
-	// replace EMBEDDER_OPENAI_API_KEY
-	reg = regexp.MustCompile(`EMBEDDER_OPENAI_API_KEY=(.*)`)
-	str = reg.ReplaceAllString(str, "EMBEDDER_OPENAI_API_KEY="+openaiApiKey)
+	// replace OPENAI_API_KEY
+	reg = regexp.MustCompile(`OPENAI_API_KEY=(.*)`)
+	str = reg.ReplaceAllString(str, "OPENAI_API_KEY="+openaiApiKey)
 
 	// replace GENERATION_MODEL
 	// it seems like using for telemetry to know the model, might be we can remove this in the future and provide a endpoint to get the information
