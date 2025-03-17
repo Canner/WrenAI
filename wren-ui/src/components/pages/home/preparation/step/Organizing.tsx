3b7faf1239d597ed2544b22c74ad342a912aec31
andreashimin <andreashimin@gmail.com>
2025-03-17 17:20:25 +0800

    chore(wren-ui): enhance auto scroll streaming

--- wren-ui/src/components/pages/home/preparation/step/Organizing.tsx
+++ wren-ui/src/components/pages/home/preparation/step/Organizing.tsx
@@ -8,11 +8,32 @@ interface Props {
 }
 
 export default function Organizing(props: Props) {
+  const $wrapper = useRef<HTMLDivElement>(null);
   const { stream, loading } = props;
+
+  const isDone = stream && !loading;
+
+  const scrollBottom = () => {
+    if ($wrapper.current) {
+      $wrapper.current.scrollTo({
+        top: $wrapper.current.scrollHeight,
+      });
+    }
+  };
+
+  useEffect(() => {
+    scrollBottom();
+  }, [stream]);
+
+  useEffect(() => {
+    if (isDone) scrollBottom();
+  }, [isDone]);
+
   return (
     <>
       <Typography.Text className="gray-8">Organizing thoughts</Typography.Text>
       <div
+        ref={$wrapper}
         className="gray-7 text-sm mt-2"
         style={{ maxHeight: 'calc(100vh - 550px)', overflowY: 'auto' }}
       >
