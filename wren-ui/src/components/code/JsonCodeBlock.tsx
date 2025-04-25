ae6106f397f8fc2c8a646daa7ca3075413f42f3e
andreashimin <andreashimin@gmail.com>
2025-04-25 18:21:42 +0800

    fix(wren-ui): defense mechanism for json string

--- wren-ui/src/components/code/JsonCodeBlock.tsx
+++ wren-ui/src/components/code/JsonCodeBlock.tsx
@@ -3,8 +3,14 @@ import { createCodeBlock, BaseProps } from './BaseCodeBlock';
 const JsonCodeBlock = (props: BaseProps) => {
   const { code, ...rest } = props;
   const { ace } = window as any;
-  const formattedJson =
-    typeof code === 'string' ? code : JSON.stringify(code, null, 2);
+  let formattedJson;
+  try {
+    formattedJson =
+      typeof code === 'string' ? code : JSON.stringify(code, null, 2);
+  } catch {
+    console.warn('Failed to format JSON', code);
+    formattedJson = code;
+  }
 
   const { JsonHighlightRules } = ace.require('ace/mode/json_highlight_rules');
   const BaseCodeBlock = createCodeBlock(JsonHighlightRules);
