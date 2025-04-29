65733d50b441b84bd1064394d28fcc8e70df40e0
ChihYu Yeh <chihyu.jimmy.yeh@gmail.com>
2025-04-29 17:17:18 +0800

    add enable_column_pruning

--- wren-ai-service/src/config.py
+++ wren-ai-service/src/config.py
@@ -29,7 +29,7 @@ class Settings(BaseSettings):
     column_indexing_batch_size: int = Field(default=50)
     table_retrieval_size: int = Field(default=10)
     table_column_retrieval_size: int = Field(default=100)
-    allow_using_db_schemas_without_pruning: bool = Field(default=False)
+    enable_column_pruning: bool = Field(default=False)
     historical_question_retrieval_similarity_threshold: float = Field(default=0.9)
     sql_pairs_similarity_threshold: float = Field(default=0.7)
     sql_pairs_retrieval_max_size: int = Field(default=10)
