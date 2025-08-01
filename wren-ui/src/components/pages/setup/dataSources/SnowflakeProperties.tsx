7fd679ae66f862e239ec0d3c12b0197da7ebc683
andreashimin <andreashimin@gmail.com>
2025-08-01 16:49:46 +0800

    feat(wren-ui): implement key pair authentication template

--- wren-ui/src/components/pages/setup/dataSources/SnowflakeProperties.tsx
+++ wren-ui/src/components/pages/setup/dataSources/SnowflakeProperties.tsx
@@ -1,6 +1,14 @@
-import { Form, Input } from 'antd';
+import Link from 'next/link';
+import { useState } from 'react';
+import { Button, Form, Input, Radio, Upload } from 'antd';
+import UploadOutlined from '@ant-design/icons/UploadOutlined';
 import { ERROR_TEXTS } from '@/utils/error';
 import { FORM_MODE } from '@/utils/enum';
+
+const TAB_KEY = {
+  PASSWORD_AUTHENTICATION: 'password_authentication',
+  KEY_PAIR_AUTHENTICATION: 'key_pair_authentication',
+};
 
 interface Props {
   mode?: FORM_MODE;
@@ -9,6 +17,12 @@ interface Props {
 export default function SnowflakeProperties(props: Props) {
   const { mode } = props;
   const isEditMode = mode === FORM_MODE.EDIT;
+  const [tabKey, setTabKey] = useState(TAB_KEY.PASSWORD_AUTHENTICATION);
+
+  const changeTabKey = (e) => {
+    setTabKey(e.target.value);
+  };
+
   return (
     <>
       <Form.Item
@@ -23,31 +37,6 @@ export default function SnowflakeProperties(props: Props) {
         ]}
       >
         <Input />
-      </Form.Item>
-      <Form.Item
-        label="Username"
-        name="user"
-        rules={[
-          {
-            required: true,
-            message: ERROR_TEXTS.CONNECTION.USERNAME.REQUIRED,
-          },
-        ]}
-      >
-        <Input />
-      </Form.Item>
-      <Form.Item
-        label="Password"
-        name="password"
-        required
-        rules={[
-          {
-            required: true,
-            message: ERROR_TEXTS.CONNECTION.PASSWORD.REQUIRED,
-          },
-        ]}
-      >
-        <Input.Password placeholder="input password" />
       </Form.Item>
       <Form.Item
         label="Account"
@@ -91,6 +80,88 @@ export default function SnowflakeProperties(props: Props) {
       >
         <Input />
       </Form.Item>
+      <Form.Item
+        label="User"
+        name="user"
+        rules={[
+          {
+            required: true,
+            message: ERROR_TEXTS.CONNECTION.USER.REQUIRED,
+          },
+        ]}
+      >
+        <Input />
+      </Form.Item>
+
+      <Form.Item
+        label={
+          <div>
+            Authentication method
+            <div className="gray-6">
+              Username and password authentication will be{' '}
+              <span className="gray-7">deprecated by November 2025</span>. We
+              recommend switching to key pair authentication.{' '}
+              <Link
+                className="gray-7 underline"
+                href="https://docs.canner.io/docs/snowflake-authentication"
+                target="_blank"
+                rel="noreferrer noopener"
+              >
+                Learn more
+              </Link>
+            </div>
+          </div>
+        }
+      >
+        <Radio.Group value={tabKey} onChange={changeTabKey} buttonStyle="solid">
+          <Radio.Button value={TAB_KEY.PASSWORD_AUTHENTICATION}>
+            Password authentication
+          </Radio.Button>
+          <Radio.Button value={TAB_KEY.KEY_PAIR_AUTHENTICATION}>
+            Key pair authentication
+          </Radio.Button>
+        </Radio.Group>
+      </Form.Item>
+
+      <div>
+        {tabKey === TAB_KEY.PASSWORD_AUTHENTICATION && (
+          <Form.Item
+            label="Password"
+            name="password"
+            required
+            rules={[
+              {
+                required: true,
+                message: ERROR_TEXTS.CONNECTION.PASSWORD.REQUIRED,
+              },
+            ]}
+          >
+            <Input.Password placeholder="input password" />
+          </Form.Item>
+        )}
+        {tabKey === TAB_KEY.KEY_PAIR_AUTHENTICATION && (
+          <Form.Item
+            label="Private key file"
+            name="privateKeyFile"
+            required
+            rules={[
+              {
+                required: true,
+                message: ERROR_TEXTS.CONNECTION.PRIVATE_KEY_FILE.REQUIRED,
+              },
+            ]}
+            extra={
+              <div className="gray-6">
+                Upload your private key file for key pair authentication.
+              </div>
+            }
+          >
+            <Upload>
+              <Button icon={<UploadOutlined />}>Upload private key</Button>
+            </Upload>
+          </Form.Item>
+        )}
+      </div>
     </>
   );
 }
