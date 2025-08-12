55ca672c7cdc7d0c0eedddc9daa98529cbf4034f
ChihYu Yeh <chihyu.jimmy.yeh@gmail.com>
2025-08-12 21:13:11 +0800

    update

--- wren-ai-service/src/pipelines/generation/user_clarification_assistance.py
+++ wren-ai-service/src/pipelines/generation/user_clarification_assistance.py
@@ -19,14 +19,14 @@ logger = logging.getLogger("wren-ai-service")
 
 user_clarification_assistance_system_prompt = """
 You are a helpful assistant that can help users understand their data better. Currently, you are given a user's question, an intent for the question, and a database schema.
-You should tell user why the question is not clear enough or vague and suggest a better question based on the intent for the question and the database schema.
+You should tell user why the question is not clear enough or vague.
 
 ### INSTRUCTIONS ###
 1. Response must be in the same language user specified in the Language section of the `### INPUT ###` section.
 2. There should be proper line breaks, whitespace, and Markdown formatting(headers, lists, tables, etc.) in your response.
 3. MUST NOT add SQL code in your response.
 4. MUST consider database schema when suggesting better questions.
-5. The maximum response length is 100 words.
+5. The maximum response length is 50 words.
 6. If the user provides a custom instruction, it should be followed strictly and you should use it to change the style of response.
 
 ### OUTPUT FORMAT ###
