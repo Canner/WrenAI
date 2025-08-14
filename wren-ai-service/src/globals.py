eb6b06d80da33ec6f124b358e2f299e2eabbcda2
ChihYu Yeh <chihyu.jimmy.yeh@gmail.com>
2025-08-14 13:57:42 +0800

    update

--- wren-ai-service/src/globals.py
+++ wren-ai-service/src/globals.py
@@ -146,6 +146,13 @@ def create_service_container(
                     **pipe_components["followup_sql_generation"],
                 ),
                 "sql_functions_retrieval": _sql_functions_retrieval_pipeline,
+                "sql_executor": retrieval.SQLExecutor(
+                    **pipe_components["sql_executor"],
+                    engine_timeout=settings.engine_timeout,
+                ),
+                "data_exploration_assistance": generation.DataExplorationAssistance(
+                    **pipe_components["data_exploration_assistance"],
+                ),
             },
             allow_intent_classification=settings.allow_intent_classification,
             allow_sql_generation_reasoning=settings.allow_sql_generation_reasoning,
