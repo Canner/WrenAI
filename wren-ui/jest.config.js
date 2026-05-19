92fc662222b86e5978d4f7fd8ba76c75b7b3762d
Harshitha <hbalasubramanya@etag.in>
2026-05-19 21:53:55 +0530

    updat questions recommad

--- wren-ui/jest.config.js
+++ wren-ui/jest.config.js
@@ -3,6 +3,7 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
     '^@server/(.*)$': '<rootDir>/src/apollo/server/$1',
   },
   modulePathIgnorePatterns: ['<rootDir>/e2e/'],
--- wren-ui/src/apollo/server/models/adaptor.ts
+++ wren-ui/src/apollo/server/models/adaptor.ts
@@ -51,6 +51,7 @@ export enum WrenAILanguage {
 export interface DeployData {
   manifest: Manifest;
   hash: string;
+  projectId: number;
 }
 
 // ask
@@ -73,6 +74,7 @@ export interface ProjectConfigurations {
 export interface AskInput {
   query: string;
   deployId: string;
+  projectId?: string;
   histories?: ThreadResponse[];
   configurations?: ProjectConfigurations;
 }
